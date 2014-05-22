129dd9c14a766ee20ed8b3090de4bf9d633b27b2
schleichardt <michael.schleichardt@commercetools.de>
2014-05-22 11:19:19 +0200

    Add test dummy class.

---
+++ test/DummyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class DummyTest {
+
+    @Test
+    public void demo() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
