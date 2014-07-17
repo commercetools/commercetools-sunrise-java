9a71c2baf78020cf622c399546ec7196730dfc96
schleichardt <michael.schleichardt@commercetools.de>
2014-07-17 15:32:32 +0200

    Show products.

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -2,9 +2,14 @@ package controllers;
 
 import io.sphere.sdk.categories.CategoryTree;
 import io.sphere.sdk.client.PlayJavaClient;
+import io.sphere.sdk.products.Product;
+import io.sphere.sdk.queries.PagedQueryResult;
+import play.libs.F;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.List;
 
 public final class ApplicationController extends SunriseController {
 
@@ -12,7 +17,12 @@ public final class ApplicationController extends SunriseController {
         super(client, categoryTree);
     }
 
-    public Result index() {
-        return ok(index.render(data().build()));
+    public F.Promise<Result> index() {
+        return client().execute(Product.query()).map(this::showProducts);
+    }
+
+    private Result showProducts(PagedQueryResult<Product> productPagedQueryResult) {
+        final List<Product> products = productPagedQueryResult.getResults();
+        return ok(index.render(data().products(products).build()));
     }
 }
--- app/models/CommonDataBuilder.java
+++ app/models/CommonDataBuilder.java
@@ -1,8 +1,10 @@
 package models;
 
 import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.products.Product;
 import play.i18n.Lang;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -10,6 +12,7 @@ public class CommonDataBuilder {
     private Lang lang;
     private List<Category> rootCategories;
     private Locale fallbackLocale = Locale.ENGLISH;
+    private List<Product> products = Collections.emptyList();
 
     private CommonDataBuilder(final Lang lang, final List<Category> rootCategories) {
         this.lang = lang;
@@ -17,7 +20,12 @@ public class CommonDataBuilder {
     }
 
     public CommonData build() {
-        return new CommonData(lang, rootCategories, fallbackLocale);
+        return new CommonData(lang, rootCategories, fallbackLocale, products);
+    }
+
+    public CommonDataBuilder products(final List<Product> products) {
+        this.products = products;
+        return this;
     }
 
     public static CommonDataBuilder of(final Lang lang, final List<Category> rootCategories) {
