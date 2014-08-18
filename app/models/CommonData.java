3588c7f4457657e721b0080a53695a48a84a9278
Laura Luiz <laura.luiz@commercetools.de>
2014-08-18 19:55:02 +0200

    Replace user context data by class in CommonData.

--- app/models/CommonData.java
+++ app/models/CommonData.java
@@ -1,37 +1,37 @@
 package models;
 
 import com.google.common.collect.Lists;
-import io.sphere.sdk.products.Product;
-import play.i18n.Lang;
 
 import java.util.List;
 import java.util.Locale;
 import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.products.Product;
 
 /**
  * A container for stuff needed in almost every template.
  */
 public class CommonData {
-    private final Lang lang;
-    private final List<Category> rootCategories;
+    private final UserContext userContext;
     private final Locale fallbackLocale;
     private final List<Locale> locales;
+    private final List<Category> rootCategories;
     private final List<Product> products;
 
-    CommonData(final Lang lang, final List<Category> rootCategories, Locale fallbackLocale, List<Product> products) {
-        this.lang = lang;
+    CommonData(final UserContext userContext, final Locale fallbackLocale, final List<Category> rootCategories,
+               final List<Product> products) {
+        this.userContext = userContext;
         this.rootCategories = rootCategories;
-        this.fallbackLocale = fallbackLocale;
         this.products = products;
-        locales = Lists.newArrayList(locale(), fallbackLocale);
+        this.fallbackLocale = fallbackLocale;
+        locales = Lists.newArrayList(userContext.locale(), fallbackLocale);
     }
 
-    public Lang lang() {
-        return lang;
+    public UserContext userContext() {
+        return userContext;
     }
 
-    public Locale locale() {
-        return lang.toLocale();
+    public Locale fallbackLocale() {
+        return fallbackLocale;
     }
 
     public List<Locale> locales() {
@@ -40,10 +40,6 @@ public class CommonData {
 
     public List<Category> rootCategories() {
         return rootCategories;
-    }
-
-    public Locale fallbackLocale() {
-        return fallbackLocale;
     }
 
     public List<Product> products() {
