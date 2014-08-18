73d301a2ba6ed6ff7cdce6fae23b8717b1693796
Laura Luiz <laura.luiz@commercetools.de>
2014-08-18 20:04:10 +0200

    Include in main controller the user context.

--- app/controllers/SunriseController.java
+++ app/controllers/SunriseController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
+import com.neovisionaries.i18n.CountryCode;
 import comparators.ByNameCategoryComparator;
 import io.sphere.sdk.categories.CategoryTree;
 import io.sphere.sdk.client.PlayJavaClient;
@@ -9,6 +10,13 @@ import io.sphere.sdk.play.controllers.ShopController;
 import models.CommonDataBuilder;
 
 import io.sphere.sdk.categories.Category;
+import models.UserContext;
+import play.Configuration;
+import play.Logger;
+import play.Play;
+import play.mvc.Http;
+
+import java.util.List;
 
 /**
  * An application specific controller.
@@ -29,6 +37,54 @@ public class SunriseController extends ShopController {
     protected final CommonDataBuilder data() {
         final ByNameCategoryComparator comparator = new ByNameCategoryComparator(lang().toLocale());
         final ImmutableList<Category> categories = Ordering.from(comparator).immutableSortedCopy(categories().getRoots());
-        return CommonDataBuilder.of(lang(), categories);
+        return CommonDataBuilder.of(userContext(), categories);
+    }
+
+    protected final UserContext userContext() {
+        return UserContext.of(lang(), country());
+    }
+
+    protected final CountryCode country() {
+        return userCountry(request(), Play.application().configuration());
+    }
+
+    /**
+     * Gets the country associated with the user.
+     * @return the country stored in the user's cookie, or the default country of the shop if none stored.
+     */
+    protected CountryCode userCountry(Http.Request request, Configuration config) {
+        Http.Cookie cookieCountry = request.cookie(countryCookieName(config));
+        if (cookieCountry != null) {
+            try {
+                return CountryCode.valueOf(cookieCountry.value());
+            } catch (IllegalArgumentException iae) {
+                Logger.debug("Invalid country defined in cookie: " + cookieCountry);
+            }
+        }
+        return defaultCountry(config);
+    }
+
+    /**
+     * Gets the default country for this shop, as defined in the configuration file.
+     * @return the first valid country defined in the configuration file.
+     */
+    protected CountryCode defaultCountry(Configuration config) {
+        List<String> configCountries = config.getStringList("sphere.countries");
+        for (String configCountry : configCountries) {
+            try {
+                return CountryCode.valueOf(configCountry);
+            } catch (IllegalArgumentException iae) {
+                Logger.error("Invalid country defined in configuration file: " + configCountry);
+            }
+        }
+        throw new RuntimeException("No valid country defined in configuration file");
+    }
+
+    /**
+     * Gets the name of the cookie containing country information.
+     * @return the name of the cookie as defined in the configuration file, or the default name if none defined.
+     */
+    protected String countryCookieName(Configuration config) {
+        return config.getString("shop.country.cookie", "SHOP_COUNTRY");
     }
 }
---
+++ test/controllers/SunriseControllerTest.java
@@ -0,0 +1,79 @@
+package controllers;
+
+import com.neovisionaries.i18n.CountryCode;
+import com.typesafe.config.ConfigFactory;
+import org.junit.Before;
+import org.junit.Test;
+import play.Configuration;
+import play.mvc.Http;
+import testUtils.TestableRequestBuilder;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class SunriseControllerTest extends WithSunriseApplication {
+
+    private SunriseController controller;
+
+    @Before
+    public void setUp() {
+        controller = new SunriseController(null, null);
+    }
+
+    @Test
+    public void userCountryIsCountryFoundInCookie() {
+        List<Http.Cookie> cookies = Collections.singletonList(countryCookie("SHOP_COUNTRY", CountryCode.ES));
+        Http.Request request = new TestableRequestBuilder().setCookies(cookies).build();
+        Configuration config = configuration(Collections.emptyMap());
+        assertThat(controller.userCountry(request, config)).isEqualTo(CountryCode.ES);
+    }
+
+    @Test
+    public void userCountryIsDefaultWhenNoCookieFound() {
+        Http.Request request = new TestableRequestBuilder().build();
+        Configuration config = configuration(Collections.singletonMap("sphere.countries", Arrays.asList("AT")));
+        assertThat(controller.userCountry(request, config)).isEqualTo(CountryCode.AT);
+    }
+
+    @Test
+    public void defaultCountryIsFirstCountryFromConfiguration() {
+        Configuration config = configuration(Collections.singletonMap("sphere.countries", Arrays.asList("AT","UK")));
+        assertThat(controller.defaultCountry(config)).isEqualTo(CountryCode.AT);
+    }
+
+    @Test
+    public void defaultCountrySkipsInvalidCountryFromConfiguration() {
+        Configuration config = configuration(Collections.singletonMap("sphere.countries", Arrays.asList("INVALID","UK")));
+        assertThat(controller.defaultCountry(config)).isEqualTo(CountryCode.UK);
+    }
+
+    @Test(expected=RuntimeException.class)
+    public void defaultCountryThrowsExceptionWhenNoneConfigured() {
+        Configuration config = configuration(Collections.emptyMap());
+        controller.defaultCountry(config);
+    }
+
+    @Test
+    public void countryCookieNameIsNameFromConfiguration() {
+        Configuration config = configuration(Collections.singletonMap("shop.country.cookie", "ANOTHER_COOKIE_NAME"));
+        assertThat(controller.countryCookieName(config)).isEqualTo("ANOTHER_COOKIE_NAME");
+    }
+
+    @Test
+    public void countryCookieNameIsDefaultWhenNoneConfigured() {
+        Configuration config = configuration(Collections.emptyMap());
+        assertThat(controller.countryCookieName(config)).isEqualTo("SHOP_COUNTRY");
+    }
+
+    private Http.Cookie countryCookie(String name, CountryCode country) {
+        return new Http.Cookie(name, country.getAlpha2(), null, null, null, false, false);
+    }
+
+    private Configuration configuration(Map<String, Object> configMap) {
+        return new Configuration(ConfigFactory.parseMap(configMap));
+    }
+}
