23c2ddf5b0b1b48069bf2469500f9894da596e58
Laura Luiz <laura.luiz@commercetools.de>
2014-10-14 17:43:56 +0200

    Add tests for CommonData class and builder.

---
+++ test/models/CommonDataTest.java
@@ -0,0 +1,44 @@
+package models;
+
+import com.neovisionaries.i18n.CountryCode;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.products.*;
+import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.producttypes.ProductTypeBuilder;
+import org.junit.Test;
+import play.i18n.Lang;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class CommonDataTest {
+
+    @Test
+    public void hasProvidedProductList() {
+        List<Product> productList = Arrays.asList(defaultProduct());
+        CommonDataBuilder builder = CommonDataBuilder.of(defaultContext(), emptyList()).products(productList);
+        assertThat(builder.build().products()).isEqualTo(productList);
+    }
+
+    @Test
+    public void hasEmptyProductListWhenNoneProvided() {
+        CommonDataBuilder builder = CommonDataBuilder.of(defaultContext(), emptyList());
+        assertThat(builder.build().products()).isEmpty();
+    }
+
+    private UserContext defaultContext() {
+        return UserContext.of(Lang.forCode("de"), CountryCode.DE);
+    }
+
+    private Product defaultProduct() {
+        ProductType productType = ProductTypeBuilder.of("id", "product-type", "", emptyList()).build();
+        LocalizedString locString = LocalizedString.ofEnglishLocale("text");
+        ProductVariant variant = ProductVariantBuilder.of(1).build();
+        ProductData productData = ProductDataBuilder.of(locString, locString, variant).build();
+        ProductCatalogData catalogData = ProductCatalogDataBuilder.ofStaged(productData).build();
+        return ProductBuilder.of(productType.toReference(), catalogData).build();
+    }
+}
