fcd4ee43513f28521892c97fe7d1d3552b4a3d8a
schleichardt <michael.schleichardt@commercetools.de>
2014-12-04 16:33:29 +0100

    Update to SDK 1.0.0-M8 in integration test scope.

--- it/controllers/ApplicationControllerIntegrationTest.java
+++ it/controllers/ApplicationControllerIntegrationTest.java
@@ -13,7 +13,7 @@ import static org.fest.assertions.Assertions.assertThat;
 public class ApplicationControllerIntegrationTest extends WithPlayJavaClient {
     @Test
     public void itFindsSomeCategories() throws Exception {
-        final PagedQueryResult<Category> queryResult = client.execute(new CategoryQuery()).get(2000);
+        final PagedQueryResult<Category> queryResult = client.execute(CategoryQuery.of()).get(2000);
         final int count = queryResult.size();
         assertThat(count).isGreaterThan(3);
         //this is a project specific assertion as example
