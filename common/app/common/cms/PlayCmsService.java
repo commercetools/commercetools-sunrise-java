adc33f91349e0c85d68415ef90692d9f52c83234
Laura Luiz <laura.luiz@commercetools.de>
2015-06-22 14:07:10 +0200

    Reference to corresponding methods in PlayCmsService javadoc

--- common/app/common/cms/PlayCmsService.java
+++ common/app/common/cms/PlayCmsService.java
@@ -11,7 +11,8 @@ import java.util.Locale;
 
 /**
  * Service that provides content data from Messages files as handled by Play.
- * In order to share a common interface with other CMS, a normal message key has been split into Page Key and Message Key.
+ * In order to share a common interface with other CMS, a normal message key has been split into Page Key
+ * (required by {@link PlayCmsService#getPage(Locale, String)}) and Message Key (required by {@link PlayCmsPage#get(String, Object...)}).
  * A possible example: "home.title" is split into "home" as Page Key and "title" as Message Key.
  * Nonetheless, you can always skip the Page Key and provide only a Message Key "home.title".
  */
