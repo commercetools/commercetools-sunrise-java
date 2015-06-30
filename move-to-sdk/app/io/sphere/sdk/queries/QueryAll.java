1e70793b5883318cbc00801e2e07a571f0ce29b3
Laura Luiz <laura.luiz@commercetools.de>
2015-06-30 16:07:20 +0200

    Implement QueryAll helper

---
+++ move-to-sdk/app/io/sphere/sdk/queries/QueryAll.java
@@ -0,0 +1,67 @@
+package io.sphere.sdk.queries;
+
+import io.sphere.sdk.client.SphereClient;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.LongStream.rangeClosed;
+
+public class QueryAll<T> {
+    private static final int DEFAULT_PAGE_SIZE = 500;
+    private final QueryDsl<T> baseQuery;
+    private final long pageSize;
+
+    private QueryAll(final QueryDsl<T> baseQuery, final long pageSize) {
+        this.baseQuery = baseQuery;
+        this.pageSize = pageSize;
+    }
+
+    public CompletionStage<List<T>> executeWith(final SphereClient client) {
+        return queryPage(client, 0).thenCompose(result -> {
+            final List<CompletableFuture<List<T>>> futureResults = new ArrayList<>();
+            futureResults.add(completedFuture(result.getResults()));
+            futureResults.addAll(queryNextPages(client, result.getTotal()));
+            return transformListOfFuturesToFutureOfLists(futureResults);
+        });
+    }
+
+    private List<CompletableFuture<List<T>>> queryNextPages(final SphereClient client, final long totalElements) {
+        final long totalPages = totalElements / pageSize;
+        return rangeClosed(1, totalPages)
+                .mapToObj(page -> queryPage(client, page)
+                        .thenApply(PagedQueryResult::getResults)
+                        .toCompletableFuture())
+                .collect(toList());
+    }
+
+    private CompletionStage<PagedQueryResult<T>> queryPage(final SphereClient client, final long pageNumber) {
+        final QueryDsl<T> query = baseQuery
+                .withOffset(pageNumber * pageSize)
+                .withLimit(pageSize);
+        return client.execute(query);
+    }
+
+    private CompletableFuture<List<T>> transformListOfFuturesToFutureOfLists(final List<CompletableFuture<List<T>>> futures) {
+        final CompletableFuture[] futuresAsArray = futures.toArray(new CompletableFuture[futures.size()]);
+        return CompletableFuture.allOf(futuresAsArray)
+                .thenApply(x -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.<T>toList()));
+    }
+
+    public static <T> QueryAll<T> of(final QueryDsl<T> baseQuery) {
+        return of(baseQuery, DEFAULT_PAGE_SIZE);
+    }
+
+    public static <T> QueryAll<T> of(final QueryDsl<T> baseQuery, final int pageSize) {
+        return new QueryAll<>(baseQuery, pageSize);
+    }
+}
---
+++ move-to-sdk/test/io/sphere/sdk/queries/QueryAllTest.java
@@ -0,0 +1,106 @@
+package io.sphere.sdk.queries;
+
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.categories.queries.CategoryQuery;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.client.SphereRequest;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Consumer;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QueryAllTest {
+    private static final int PAGE_SIZE = 5;
+
+    @Test
+    public void onEmptyResult() throws Exception {
+        withClient(clientWithResults(0), list -> assertThat(list).isSorted().isEmpty());
+    }
+
+    @Test
+    public void onOnePageResult() throws Exception {
+        withClient(clientWithResults(3), list -> assertThat(list).isSorted().hasSize(3));
+    }
+
+    @Test
+    public void onMultiplePagesResult() throws Exception {
+        withClient(clientWithResults(16), list -> assertThat(list).isSorted().hasSize(16));
+    }
+
+    @Test
+    public void onWrongTotalResult() throws Exception {
+        withClient(clientWithWrongResults(16, 10), list -> assertThat(list).isSorted().hasSize(16));
+    }
+
+    @Test
+    public void onUnsortedResponses() throws Exception {
+        withClient(clientWithDelays(16), list -> assertThat(list).isSorted().hasSize(16));
+    }
+
+    private void withClient(final SphereClient client, final Consumer<List> test) {
+        final QueryAll<Category> query = QueryAll.of(CategoryQuery.of(), 5);
+        final List elements = query.executeWith(client).toCompletableFuture().join();
+        test.accept(elements);
+    }
+
+    private SphereClient clientWithResults(final int totalResults) {
+        return client(totalResults, 0, false);
+    }
+
+    private SphereClient clientWithWrongResults(final int totalResults, final int deviation) {
+        return client(totalResults, deviation, false);
+    }
+
+    private SphereClient clientWithDelays(final int totalResults) {
+        return client(totalResults, 0, true);
+    }
+
+    private SphereClient client(final int totalResults, final int deviation, final boolean withDelays) {
+        return new SphereClient() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public <T> CompletionStage<T> execute(final SphereRequest<T> request) {
+                final QueryDslImpl<T> query = (QueryDslImpl<T>) request;
+                final int offset = query.offset().get().intValue();
+                return CompletableFuture.supplyAsync(() -> {
+                    if (withDelays) {
+                        try {
+                            Thread.sleep(100 / (offset + 1));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    return (T) generatePagedQueryResult(offset);
+                });
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @SuppressWarnings("unchecked")
+            private <T> PagedQueryResult<T> generatePagedQueryResult(final int offset) {
+                final int total = totalResults + deviation;
+                final int count = min(PAGE_SIZE, max(totalResults - offset, 0));
+                final List<T> results = (List<T>) generateSortedResultList(offset, count);
+                return PagedQueryResult.of(offset, total, results);
+            }
+
+            private List<Integer> generateSortedResultList(final int offset, final int count) {
+                return range(offset, offset + count)
+                        .boxed()
+                        .collect(toList());
+            }
+        };
+    }
+}
