5698b0ef451d90b9f29e645261ab7a06a8071410
Laura Luiz <laura.luiz@commercetools.de>
2015-06-30 17:05:54 +0200

    Inject all categories

---
+++ app/inject/SunriseInitializationException.java
@@ -0,0 +1,8 @@
+package inject;
+
+public class SunriseInitializationException extends RuntimeException {
+
+    public SunriseInitializationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
