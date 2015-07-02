035d5e38577cf8454da732279670eadc35a4eb88
Laura Luiz <laura.luiz@commercetools.de>
2015-07-02 18:51:40 +0200

    Change name client for sphere

--- common/app/common/controllers/ControllerDependency.java
+++ common/app/common/controllers/ControllerDependency.java
@@ -11,24 +11,24 @@ import javax.inject.Singleton;
 
 @Singleton
 public class ControllerDependency {
-    private final PlayJavaSphereClient client;
+    private final PlayJavaSphereClient sphere;
     private final CategoryTree categoryTree;
     private final ProjectContext projectContext;
     private final TemplateService templateService;
     private final CmsService cmsService;
 
     @Inject
-    public ControllerDependency(final PlayJavaSphereClient client, final CategoryTree categoryTree,
+    public ControllerDependency(final PlayJavaSphereClient sphere, final CategoryTree categoryTree,
                                 final ProjectContext projectContext, final TemplateService templateService, final CmsService cmsService) {
-        this.client = client;
+        this.sphere = sphere;
         this.categoryTree = categoryTree;
         this.projectContext = projectContext;
         this.templateService = templateService;
         this.cmsService = cmsService;
     }
 
-    public PlayJavaSphereClient client() {
-        return client;
+    public PlayJavaSphereClient sphere() {
+        return sphere;
     }
 
     public CategoryTree categoryTree() {
