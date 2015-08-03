cbd43d22f91332f70843cc8f62f927d775e6cd98
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-03 14:16:54 +0200

    Adjust tests to use TestData

--- product-catalog/test/productcatalog/pages/ProductDetailPageContentTest.java
+++ product-catalog/test/productcatalog/pages/ProductDetailPageContentTest.java
@@ -5,34 +5,33 @@ import common.pages.LinkData;
 import common.utils.Translator;
 import io.sphere.sdk.categories.Category;
 import io.sphere.sdk.categories.CategoryTree;
-import io.sphere.sdk.categories.queries.CategoryQuery;
 import io.sphere.sdk.models.Image;
 import io.sphere.sdk.models.ImageDimensions;
 import io.sphere.sdk.products.ProductProjection;
-import io.sphere.sdk.products.ProductVariant;
-import io.sphere.sdk.products.queries.ProductProjectionQuery;
 import org.junit.Test;
+import productcatalog.common.CategoryTestData;
+import productcatalog.common.ProductTestData;
 
 import java.util.List;
 import java.util.Locale;
 
-import static io.sphere.sdk.json.JsonUtils.readObjectFromResource;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductDetailPageContentTest {
 
     private final Translator translator = Translator.of(Locale.ENGLISH, emptyList(), emptyList());
 
-    private final ProductProjection product = readObjectFromResource("productProjectionQuery.json", ProductProjectionQuery.resultTypeReference()).head().get();
-    private final ProductVariant variant = product.getMasterVariant();
-    private final CategoryTree categories = CategoryTree.of(readObjectFromResource("categories.json", CategoryQuery.resultTypeReference()).getResults());
+    private final List<ProductProjection> products = ProductTestData.of().getProducts();
+    private final CategoryTree categories = CategoryTree.of(CategoryTestData.of().getCategories());
 
     @Test
     public void additionalTitle() throws Exception {
-        final ProductDetailPageContent content = new ProductDetailPageContent(null, translator, null, null, product, null, null, null, null);
+        final ProductProjection suttonBag = products.get(6);
+        final ProductDetailPageContent content = new ProductDetailPageContent(null, translator, null, null, suttonBag, null, null, null, null);
 
+        assertThat(suttonBag.getId()).isEqualTo("254f1c0e-67bc-4aa6-992d-9a0fea1846b5");
         assertThat(content.additionalTitle()).isEqualTo("Bag ”Sutton” large Michael Kors");
     }
 
@@ -51,15 +50,15 @@ public class ProductDetailPageContentTest {
 
     @Test
     public void getGallery() throws Exception {
-        final Image image1 = Image.of("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/086652_1_large.jpg", ImageDimensions.of(0, 0));
-        final Image image2 = Image.of("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/086652_2_large.jpg", ImageDimensions.of(0, 0));
-        final Image image3 = Image.of("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/086652_3_large.jpg", ImageDimensions.of(0, 0));
+        final ProductProjection maximilian16 = products.get(0);
+        final Image image1 = Image.of("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/s15lookd16_1_medium.jpg", ImageDimensions.of(0, 0));
+        final Image image2 = Image.of("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/s15lookd16_2_medium.jpg", ImageDimensions.of(0, 0));
+        final Image image3 = Image.of("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/s15lookd16_3_medium.jpg", ImageDimensions.of(0, 0));
 
-        final ProductDetailPageContent content = new ProductDetailPageContent(null, null, null, null, null, variant, null, null, null);
+        final ProductDetailPageContent content = new ProductDetailPageContent(null, null, null, null, null, maximilian16.getMasterVariant(), null, null, null);
         final List<ImageData> expectedResult = asList(ImageData.of(image1), ImageData.of(image2), ImageData.of(image3));
 
+        assertThat(maximilian16.getId()).isEqualTo("2a439c1c-1687-4499-9d5a-8d5e3e6620a4");
         assertThat(content.getGallery()).isEqualTo(expectedResult);
     }
-
-
 }
--- product-catalog/test/productcatalog/services/CategoryServiceTest.java
+++ product-catalog/test/productcatalog/services/CategoryServiceTest.java
@@ -2,36 +2,35 @@ package productcatalog.services;
 
 import io.sphere.sdk.categories.Category;
 import io.sphere.sdk.categories.CategoryTree;
-import io.sphere.sdk.categories.queries.CategoryQuery;
 import io.sphere.sdk.products.ProductProjection;
-import io.sphere.sdk.products.queries.ProductProjectionQuery;
 import org.junit.Test;
+import productcatalog.common.CategoryTestData;
+import productcatalog.common.ProductTestData;
 
 import java.util.List;
 
-import static io.sphere.sdk.json.JsonUtils.readObjectFromResource;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CategoryServiceTest {
 
-    private final CategoryTree categories = CategoryTree.of(readObjectFromResource("categories.json", CategoryQuery.resultTypeReference()).getResults());
-    private final ProductProjection product = readObjectFromResource("productProjectionQuery.json", ProductProjectionQuery.resultTypeReference()).head().get();
+    private final CategoryTree categories = CategoryTree.of(CategoryTestData.of().getCategories());
+    private final List<ProductProjection> products = ProductTestData.of().getProducts();
     private final CategoryService categrieService = new CategoryService(categories);
 
     @Test
     public void getBreadCrumbCategories() {
 
+        final ProductProjection suttonBag = products.get(6);
+
         final Category woman = categories.findById("33339d11-0e7b-406b-899b-60f4c34c2948").get();
         final Category bags = categories.findById("32952779-d916-4f2b-b1d5-9efd7f7b9f58").get();
+        final Category shoulderBags = categories.findById("30d79426-a17a-4e63-867e-ec31a1a33416").get();
 
-        assertThat(product.getCategories()).contains(bags.toReference());
-        assertThat(bags.getParent()).contains(woman.toReference());
-        assertThat(woman.getParent()).isEmpty();
+        final List<Category> expectedResult = asList(woman, bags, shoulderBags);
+        final List<Category> result = categrieService.getBreadCrumbCategories(suttonBag);
 
-        final List<Category> expectedResult = asList(woman, bags);
-        final List<Category> result = categrieService.getBreadCrumbCategories(product);
-
+        assertThat(suttonBag.getId()).isEqualTo("254f1c0e-67bc-4aa6-992d-9a0fea1846b5");
         assertThat(result).isEqualTo(expectedResult);
     }
 }
