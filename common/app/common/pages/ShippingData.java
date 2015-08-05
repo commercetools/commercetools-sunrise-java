3933f65004eec6beedd715e52b0288d257806e61
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-05 12:11:17 +0200

    Add ShippingData model

---
+++ common/app/common/pages/ShippingData.java
@@ -0,0 +1,36 @@
+package common.pages;
+
+public class ShippingData {
+
+    private final String shippingMethod;
+    private final String zone;
+    private final String shippingRate;
+    private final String freeAbove;
+
+    public ShippingData(final String shippingMethod, final String zone, final String shippingRate, final String freeAbove) {
+        this. shippingMethod = shippingMethod;
+        this.zone = zone;
+        this.shippingRate = shippingRate;
+        this.freeAbove = freeAbove;
+    }
+
+    public String getText() {
+        return String.format("%s %s %s %s", shippingMethod, zone, shippingRate, freeAbove);
+    }
+
+    public String getShippingMethod() {
+        return shippingMethod;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public String getShippingRate() {
+        return shippingRate;
+    }
+
+    public String getFreeAbove() {
+        return freeAbove;
+    }
+}
