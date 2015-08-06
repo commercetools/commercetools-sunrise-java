fdcfd2c11b5dcd8f2402196c206b16391030d0c7
Laura Luiz <laura.luiz@commercetools.de>
2015-08-06 20:15:03 +0200

    Enable context fallback for handlebars

--- common/app/common/templates/HandlebarsTemplateService.java
+++ common/app/common/templates/HandlebarsTemplateService.java
@@ -1,26 +1,38 @@
 package common.templates;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.context.MapValueResolver;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
+
 import common.pages.PageData;
 import play.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
 
 public final class HandlebarsTemplateService implements TemplateService {
     private final Handlebars handlebars;
+    private final List<TemplateLoader> fallbackContexts;
 
-    private HandlebarsTemplateService(final Handlebars handlebars) {
+    private HandlebarsTemplateService(final Handlebars handlebars, final List<TemplateLoader> fallbackContexts) {
         this.handlebars = handlebars;
+        this.fallbackContexts = fallbackContexts;
     }
 
     @Override
     public String render(final String templateName, final PageData pageData) {
         final Template template = compileTemplate(templateName);
-        final Context context = buildContext(pageData);
+        final Context context = buildContext(pageData, templateName);
         try {
             Logger.debug("Rendering template " + templateName);
             return template.apply(context);
@@ -29,9 +41,14 @@ public final class HandlebarsTemplateService implements TemplateService {
         }
     }
 
-    public static HandlebarsTemplateService of(final TemplateLoader... loaders) {
+    public static TemplateService of(final List<TemplateLoader> templateLoaders) {
+        return of(templateLoaders, emptyList());
+    }
+
+    public static TemplateService of(final List<TemplateLoader> templateLoaders, final List<TemplateLoader> fallbackContexts) {
+        final TemplateLoader[] loaders = templateLoaders.toArray(new TemplateLoader[templateLoaders.size()]);
         final Handlebars handlebars = new Handlebars().with(loaders);
-        return new HandlebarsTemplateService(handlebars);
+        return new HandlebarsTemplateService(handlebars, fallbackContexts);
     }
 
     private Template compileTemplate(final String templateName) {
@@ -42,17 +59,38 @@ public final class HandlebarsTemplateService implements TemplateService {
         }
     }
 
-    private Context buildContext(final PageData pageData) {
+    private Context buildContext(final PageData pageData, final String templateName) {
         // TODO Use resolver with cache on production
-        return Context.newBuilder(pageData)
-                .resolver(NonCachedJavaBeanValueResolver.INSTANCE)
-                .build();
+        Context.Builder builder = Context.newBuilder(pageData)
+                .resolver(NonCachedJavaBeanValueResolver.INSTANCE, MapValueResolver.INSTANCE);
+        for (final TemplateLoader fallbackContext : fallbackContexts) {
+            final Optional<Map<String, ?>> map = buildFallbackContext(fallbackContext, templateName);
+            if (map.isPresent()) {
+                builder = builder.combine(map.get());
+            }
+        }
+        return builder.build();
     }
 
+    private Optional<Map<String, ?>> buildFallbackContext(final TemplateLoader fallbackLoader, final String templateName) {
+        return getResource(fallbackLoader, templateName).map(stream -> {
+            try {
+                return new ObjectMapper().readValue(stream, HashMap.class);
+            } catch (IOException e) {
+                Logger.error("Could not read fallback context", e);
+                return null;
+            }
+        });
+    }
 
-    public static TemplateService of(final List<TemplateLoader> templateLoaders) {
-        final TemplateLoader[] templateLoadersArray =
-                templateLoaders.toArray(new TemplateLoader[templateLoaders.size()]);
-        return of(templateLoadersArray);
+    private Optional<InputStream> getResource(final TemplateLoader fallbackLoader, final String templateName) {
+        final InputStream resource;
+        if (fallbackLoader instanceof ClassPathTemplateLoader) {
+            final String path = fallbackLoader.resolve(templateName).replaceAll("^/?(.*)\\.hbs$", "$1.json");
+            resource = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+        } else {
+            throw new RuntimeException("Invalid fallback context type, only classpath supported");
+        }
+        return Optional.ofNullable(resource);
     }
 }
