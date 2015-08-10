e0c8c951ec6b805adbd785c12865d1b9b4937fb5
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-10 13:41:28 +0200

    Remove CmsPage dependency from PageContent

--- common/app/common/pages/PageContent.java
+++ common/app/common/pages/PageContent.java
@@ -1,13 +1,6 @@
 package common.pages;
 
-import common.cms.CmsPage;
-
 public abstract class PageContent {
-    protected final CmsPage cms;
-
-    public PageContent(final CmsPage cms) {
-        this.cms = cms;
-    }
 
     public abstract String additionalTitle();
 }
