f4787a6eae43bff7d17facb01a865463b9f45cad
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-17 17:50:25 +0200

    Add RatingDataFactoryTest

---
+++ common/test/common/pages/RatingDataFactoryTest.java
@@ -0,0 +1,24 @@
+package common.pages;
+
+import common.cms.CmsPage;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Optional;
+
+public class RatingDataFactoryTest {
+    
+    @Test
+    public void create() {
+        final CmsPage cms = (messageKey, args) -> Optional.of(messageKey);
+
+        final RatingData ratingData = RatingDataFactory.of(cms).create();
+
+        Assertions.assertThat(ratingData.getRating()).containsExactly(
+                new SelectableData("5 Stars", "5", "ratingFiveStarText", "", false),
+                new SelectableData("4 Stars", "4", "ratingFourStarText", "", false),
+                new SelectableData("3 Stars", "3", "ratingThreeStarText", "", false),
+                new SelectableData("2 Stars", "2", "ratingTwoStarText", "", false),
+                new SelectableData("1 Stars", "1", "ratingOneStarText", "", false));
+    }
+}
