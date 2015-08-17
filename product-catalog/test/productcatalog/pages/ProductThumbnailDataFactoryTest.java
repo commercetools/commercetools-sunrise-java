c115848a1a455da1ed80c21f5383833a66018cf7
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-17 16:16:56 +0200

    Add ProductThumbnailDataFactoryTest

---
+++ product-catalog/test/productcatalog/pages/ProductThumbnailDataFactoryTest.java
@@ -0,0 +1,40 @@
+package productcatalog.pages;
+
+import com.neovisionaries.i18n.CountryCode;
+import common.pages.ProductThumbnailData;
+import common.pages.ProductThumbnailDataFactory;
+import common.prices.PriceFinder;
+import common.utils.PriceFormatter;
+import common.utils.Translator;
+import io.sphere.sdk.products.ProductProjection;
+import org.junit.Test;
+
+import javax.money.Monetary;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import static common.products.ProductUtils.getQueryResult;
+import static io.sphere.sdk.json.JsonUtils.readObjectFromResource;
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductThumbnailDataFactoryTest {
+    private final Translator translator = Translator.of(Locale.GERMAN, emptyList(), emptyList());
+    private final PriceFinder priceFinder = PriceFinder.of(Monetary.getCurrency("EUR"), CountryCode.DE, Optional.empty(), Optional.empty());
+    private final PriceFormatter priceFormatter = PriceFormatter.of(Locale.GERMAN);
+    private final List<ProductProjection> products = getQueryResult("productProjectionQueryResult.json").getResults();
+
+    @Test
+    public void create() {
+        final ProductProjection jacket = readObjectFromResource("product.json", ProductProjection.typeReference());
+
+        final ProductThumbnailData thumbnailData =
+                ProductThumbnailDataFactory.of(translator, priceFinder, priceFormatter).create(jacket);
+
+        assertThat(thumbnailData.getText()).isEqualTo("Freizeitjacke Save the Duck oliv");
+        assertThat(thumbnailData.getDescription()).isEqualTo("german test description");
+        assertThat(thumbnailData.getImage()).isEqualTo("https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/078686_1_large.jpg");
+        assertThat(thumbnailData.getPrice()).isEqualTo("EUR 129,00");
+    }
+}
