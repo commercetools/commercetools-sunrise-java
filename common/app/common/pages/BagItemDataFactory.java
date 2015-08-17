1b6483696daea09e00859135194d08d36aea671a
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-17 12:29:11 +0200

    Fix BagItemDataFactory and tests

--- common/app/common/pages/BagItemDataFactory.java
+++ common/app/common/pages/BagItemDataFactory.java
@@ -31,7 +31,7 @@ public class BagItemDataFactory {
         final List<SelectableData> bagItems = new ArrayList<>(numItems);
         bagItems.add(bagItemDefaultItem());
 
-        for(int i = 0; i < (numItems - 1); i++) {
+        for(int i = 2; i <= numItems; i++) {
             bagItems.add(this.bagItemToSelectableItem(i));
         }
 
