805f4ffc43b1d71dd986a842e92644dbc0cfbb4b
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-17 14:02:25 +0200

    Move Translator, PriceFinder and PriceFormatter into context

--- common/app/common/contexts/UserContext.java
+++ common/app/common/contexts/UserContext.java
@@ -1,6 +1,9 @@
 package common.contexts;
 
 import com.neovisionaries.i18n.CountryCode;
+import common.prices.PriceFinder;
+import common.utils.PriceFormatter;
+import common.utils.Translator;
 import io.sphere.sdk.channels.Channel;
 import io.sphere.sdk.customergroups.CustomerGroup;
 import io.sphere.sdk.models.Reference;
@@ -8,7 +11,9 @@ import io.sphere.sdk.zones.Zone;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -24,6 +29,9 @@ public class UserContext {
     private final Reference<Zone> zone;
     private final Optional<Reference<CustomerGroup>> customerGroup;
     private final Optional<Reference<Channel>> channel;
+    private final CurrencyUnit currency;
+    private final PriceFinder priceFinder;
+    private final PriceFormatter priceFormatter;
 
     private UserContext(final CountryCode country, final Locale language, final List<Locale> fallbackLanguages, final ZoneId zoneId, final Reference<Zone> zone, final Reference<CustomerGroup> customerGroup, final Reference<Channel> channel) {
         this.country = country;
@@ -33,6 +41,9 @@ public class UserContext {
         this.zone = zone;
         this.customerGroup = Optional.ofNullable(customerGroup);
         this.channel = Optional.ofNullable(channel);
+        this.currency = Monetary.getCurrency("EUR");
+        this.priceFinder = PriceFinder.of(currency, country, this.customerGroup, this.channel);
+        this.priceFormatter = PriceFormatter.of(language);
     }
 
     public static UserContext of(final CountryCode country, final Locale language, final List<Locale> fallbackLanguages, final ZoneId zoneId, final Reference<Zone> zone) {
@@ -64,7 +75,7 @@ public class UserContext {
     }
 
     public CurrencyUnit currency() {
-        return Monetary.getCurrency("EUR");
+        return currency;
     }
 
     public Optional<Reference<CustomerGroup>> customerGroup() {
@@ -73,5 +84,13 @@ public class UserContext {
 
     public Optional<Reference<Channel>> channel() {
         return channel;
+    }
+
+    public PriceFinder priceFinder() {
+        return priceFinder;
+    }
+
+    public PriceFormatter priceFormatter() {
+        return priceFormatter;
     }
 }
