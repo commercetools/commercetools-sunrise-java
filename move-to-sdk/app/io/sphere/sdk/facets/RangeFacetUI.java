ede48cdf03cafbeab2cc54bffc7b696ec01a1ca4
Laura Luiz <laura.luiz@commercetools.de>
2015-08-18 20:29:43 +0200

    Improve draft for Range facets

--- move-to-sdk/app/io/sphere/sdk/facets/RangeFacetUI.java
+++ move-to-sdk/app/io/sphere/sdk/facets/RangeFacetUI.java
@@ -1,29 +1,47 @@
 package io.sphere.sdk.facets;
 
-import io.sphere.sdk.search.FacetExpression;
 import io.sphere.sdk.search.RangeFacetResult;
 
-public class RangeFacetUI<T, V> extends BaseFacetUI<T> {
-    private final RangeFacetResult<V> facetResult;
+public class RangeFacetUI<T extends Comparable<? super T>> extends BaseFacetUI {
+    private final T minSelectedValue;
+    private final T maxSelectedValue;
+    private final RangeFacetResult<T> facetResult;
     private final long unit;
 
-    public RangeFacetUI(final String key, final String label, final FacetExpression<T> expression,
-                        final RangeFacetResult<V> facetResult, final long unit) {
-        super(key, label, expression);
+    protected RangeFacetUI(final String key, final String label, final RangeFacetResult<T> facetResult,
+                           final T minSelectedValue, final T maxSelectedValue, final long unit) {
+        super(key, label);
+        this.minSelectedValue = minSelectedValue;
+        this.maxSelectedValue = maxSelectedValue;
         this.facetResult = facetResult;
         this.unit = unit;
     }
 
-    public RangeFacetResult<V> getFacetResult() {
+    @Override
+    public RangeFacetResult<T> getFacetResult() {
         return facetResult;
+    }
+
+    @Override
+    public boolean canBeDisplayed() {
+        return !facetResult.getRanges().isEmpty();
+    }
+
+    public T getMinSelectedValue() {
+        return minSelectedValue;
+    }
+
+    public T getMaxSelectedValue() {
+        return maxSelectedValue;
     }
 
     public long getUnit() {
         return unit;
     }
 
-    @Override
-    public boolean canBeDisplayed() {
-        return !facetResult.getRanges().isEmpty();
+    public static <T extends Comparable<? super T>> RangeFacetUI<T> of(final String key, final String label,
+                                                                       final T minSelectedValue, final T maxSelectedValue,
+                                                                       final RangeFacetResult<T> facetResult, final long unit) {
+        return new RangeFacetUI<>(key, label, facetResult, minSelectedValue, maxSelectedValue, unit);
     }
 }
