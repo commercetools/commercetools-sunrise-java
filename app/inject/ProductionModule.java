aec05a9983acdd66e783591ebe31148cd5be64b4
schleichardt <michael.schleichardt@commercetools.de>
2015-08-19 09:57:56 +0200

    Simplify ShippingMethodService instantiation.

--- app/inject/ProductionModule.java
+++ app/inject/ProductionModule.java
@@ -6,12 +6,10 @@ import common.templates.TemplateService;
 import io.sphere.sdk.categories.CategoryTree;
 import io.sphere.sdk.client.PlayJavaSphereClient;
 import io.sphere.sdk.client.SphereClient;
-import productcatalog.models.ShippingMethods;
 import play.api.Environment;
 import play.api.inject.Binding;
 import play.api.inject.Module;
 import productcatalog.services.CategoryService;
-import productcatalog.services.CategoryServiceImpl;
 import productcatalog.services.ProductProjectionService;
 import productcatalog.services.ShippingMethodService;
 import scala.collection.Seq;
@@ -33,7 +31,6 @@ public class ProductionModule extends Module {
                 bind(CategoryTree.class).toProvider(CategoryTreeProvider.class).in(Singleton.class),
                 bind(TemplateService.class).toProvider(TemplateServiceProvider.class).in(Singleton.class),
                 bind(CmsService.class).toProvider(CmsServiceProvider.class).in(Singleton.class),
-                bind(ShippingMethods.class).toProvider(ShippingMethodProvider.class).in(Singleton.class),
                 bind(CategoryService.class).toProvider(CategoryServiceProvider.class).in(Singleton.class),
                 bind(ProductProjectionService.class).toProvider(ProductProjectionServiceProvider.class).in(Singleton.class),
                 bind(ShippingMethodService.class).toProvider(ShippingMethodServiceProvider.class).in(Singleton.class)
