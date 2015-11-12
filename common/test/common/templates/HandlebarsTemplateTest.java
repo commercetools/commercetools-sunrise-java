fe031640230ea10998b1153d713064f24c153540
schleichardt <michael.schleichardt@commercetools.de>
2015-11-12 15:28:40 +0100

    Remove comments.

--- common/test/common/templates/HandlebarsTemplateTest.java
+++ common/test/common/templates/HandlebarsTemplateTest.java
@@ -25,93 +25,93 @@ public class HandlebarsTemplateTest {
             "  bundles=[\"translations\", \"home\", \"catalog\", \"checkout\", \"foo\"]\n" +
             "}");
 
-//    @Test
-//    public void rendersTemplateWithPartial() throws Exception {
-//        final String html = handlebars().render("template", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("<title>foo</title>")
-//                .contains("<h1>bar</h1>")
-//                .contains("<h2></h2>")
-//                .contains("<p>default partial</p>")
-//                .contains("<ul></ul>");
-//    }
-//
-//    @Test
-//    public void rendersOverriddenTemplateUsingOverriddenAndDefaultPartials() throws Exception {
-//        final String html = handlebarsWithOverride().render("template", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("overridden template")
-//                .contains("overridden partial")
-//                .contains("another default partial");
-//    }
-//
-//    @Test
-//    public void rendersDefaultTemplateUsingOverriddenAndDefaultPartials() throws Exception {
-//        final String html = handlebarsWithOverride().render("anotherTemplate", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("default template")
-//                .contains("overridden partial")
-//                .contains("another default partial");
-//    }
-//
-//    @Test
-//    public void throwsExceptionWhenTemplateNotFound() throws Exception {
-//        assertThatThrownBy(() -> handlebars().render("unknown", pageDataWithTitleAndMessage(), LOCALES))
-//                .isInstanceOf(TemplateNotFoundException.class);
-//    }
-//
-//    @Test
-//    public void usesFallbackContextWhenMissingData() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("template", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("<title>foo</title>")
-//                .contains("<h1>bar</h1>")
-//                .contains("<h2>fallback unknown</h2>")
-//                .contains("<p>default partial</p>")
-//                .contains("<ul><li>fallback foo</li><li>fallback bar</li></ul>");
-//    }
-//
-//    @Test
-//    public void failsSilentlyWhenFallbackContextNotFound() throws Exception {
-//        final String html = handlebarsWithFallbackContext(WRONG_LOADER).render("template", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("<title>foo</title>")
-//                .contains("<h1>bar</h1>")
-//                .contains("<h2></h2>")
-//                .contains("<p>default partial</p>")
-//                .contains("<ul></ul>");
-//    }
-//
-//    @Test
-//    public void simpleTranslation() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/simple", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("Sales Tax");
-//    }
-//
-//    @Test
-//    public void simpleTranslationWithBundle() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/simpleBundle", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("Secure Checkout - Confirmation");
-//    }
-//
-//    @Test
-//    public void translationWithParameter() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/parameter", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).contains("I agree to the <a id=\"confirmation-termsandconditions-link\" href=\"http://www.commercetools.com/de/\"> <span class=\"terms-link\">Terms and Conditions</span></a>");
-//    }
-//
-//    @Test
-//    public void notFound() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/missing", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).isEqualTo("");
-//    }
-//
-//    @Test
-//    public void notFoundInBundle() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/missingKeyInBundle", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).isEqualTo("");
-//    }
-//
-//    @Test
-//    public void bundleNotFound() throws Exception {
-//        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/missingBundle", pageDataWithTitleAndMessage(), LOCALES);
-//        assertThat(html).isEqualTo("");
-//    }
+    @Test
+    public void rendersTemplateWithPartial() throws Exception {
+        final String html = handlebars().render("template", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("<title>foo</title>")
+                .contains("<h1>bar</h1>")
+                .contains("<h2></h2>")
+                .contains("<p>default partial</p>")
+                .contains("<ul></ul>");
+    }
+
+    @Test
+    public void rendersOverriddenTemplateUsingOverriddenAndDefaultPartials() throws Exception {
+        final String html = handlebarsWithOverride().render("template", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("overridden template")
+                .contains("overridden partial")
+                .contains("another default partial");
+    }
+
+    @Test
+    public void rendersDefaultTemplateUsingOverriddenAndDefaultPartials() throws Exception {
+        final String html = handlebarsWithOverride().render("anotherTemplate", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("default template")
+                .contains("overridden partial")
+                .contains("another default partial");
+    }
+
+    @Test
+    public void throwsExceptionWhenTemplateNotFound() throws Exception {
+        assertThatThrownBy(() -> handlebars().render("unknown", pageDataWithTitleAndMessage(), LOCALES))
+                .isInstanceOf(TemplateNotFoundException.class);
+    }
+
+    @Test
+    public void usesFallbackContextWhenMissingData() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("template", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("<title>foo</title>")
+                .contains("<h1>bar</h1>")
+                .contains("<h2>fallback unknown</h2>")
+                .contains("<p>default partial</p>")
+                .contains("<ul><li>fallback foo</li><li>fallback bar</li></ul>");
+    }
+
+    @Test
+    public void failsSilentlyWhenFallbackContextNotFound() throws Exception {
+        final String html = handlebarsWithFallbackContext(WRONG_LOADER).render("template", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("<title>foo</title>")
+                .contains("<h1>bar</h1>")
+                .contains("<h2></h2>")
+                .contains("<p>default partial</p>")
+                .contains("<ul></ul>");
+    }
+
+    @Test
+    public void simpleTranslation() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/simple", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("Sales Tax");
+    }
+
+    @Test
+    public void simpleTranslationWithBundle() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/simpleBundle", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("Secure Checkout - Confirmation");
+    }
+
+    @Test
+    public void translationWithParameter() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/parameter", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).contains("I agree to the <a id=\"confirmation-termsandconditions-link\" href=\"http://www.commercetools.com/de/\"> <span class=\"terms-link\">Terms and Conditions</span></a>");
+    }
+
+    @Test
+    public void notFound() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/missing", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).isEqualTo("");
+    }
+
+    @Test
+    public void notFoundInBundle() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/missingKeyInBundle", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).isEqualTo("");
+    }
+
+    @Test
+    public void bundleNotFound() throws Exception {
+        final String html = handlebarsWithFallbackContext(DEFAULT_LOADER).render("translations/missingBundle", pageDataWithTitleAndMessage(), LOCALES);
+        assertThat(html).isEqualTo("");
+    }
 
     @Test
     public void plural() throws Exception {
