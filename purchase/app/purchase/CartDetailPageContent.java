2d48a30b251116781564abf10474de9effdbf641
schleichardt <michael.schleichardt@commercetools.de>
2015-11-13 11:50:40 +0100

    Add CartOrderBean. #194

--- purchase/app/purchase/CartDetailPageContent.java
+++ purchase/app/purchase/CartDetailPageContent.java
@@ -4,16 +4,17 @@ import common.contexts.UserContext;
 import common.pages.PageContent;
 import common.pages.ReverseRouter;
 import io.sphere.sdk.carts.Cart;
-import io.sphere.sdk.carts.LineItem;
 import play.i18n.Messages;
 
 public class CartDetailPageContent extends PageContent {
     private final CartOrderBean cart;
 
     public CartDetailPageContent(final Cart cart, final UserContext userContext, final Messages messages, final ReverseRouter reverseRouter) {
-        this.cart = new CartOrderBean();
-        final long totalItems = cart.getLineItems().stream().mapToLong(LineItem::getQuantity).sum();
-        this.cart.setItemsTotal(totalItems);
+        this.cart = new CartOrderBean(cart);
+    }
+
+    public CartOrderBean getCart() {
+        return cart;
     }
 
     @Override
--- purchase/app/purchase/CartOrderBean.java
+++ purchase/app/purchase/CartOrderBean.java
@@ -1,7 +1,18 @@
 package purchase;
 
+import io.sphere.sdk.carts.CartLike;
+import io.sphere.sdk.carts.LineItem;
+
 public class CartOrderBean {
     private Long itemsTotal;
+
+    public CartOrderBean() {
+    }
+
+    public CartOrderBean(final CartLike<?> cartLike) {
+        this();
+        setItemsTotal(cartLike.getLineItems().stream().mapToLong(LineItem::getQuantity).sum());
+    }
 
     public Long getItemsTotal() {
         return itemsTotal;
