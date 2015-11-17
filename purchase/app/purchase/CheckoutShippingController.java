ac03c0cf308d78e717ad423a9b14b3730f45ec86
schleichardt <michael.schleichardt@commercetools.de>
2015-11-17 17:13:31 +0100

    Add csrf protections for checkout shipping. #190

--- purchase/app/purchase/CheckoutShippingController.java
+++ purchase/app/purchase/CheckoutShippingController.java
@@ -5,36 +5,35 @@ import common.controllers.ControllerDependency;
 import common.models.ProductDataConfig;
 import common.pages.SunrisePageData;
 import io.sphere.sdk.carts.Cart;
-import play.filters.csrf.CSRF;
+import play.filters.csrf.AddCSRFToken;
 import play.filters.csrf.RequireCSRFCheck;
 import play.i18n.Messages;
 import play.libs.F;
 import play.mvc.Result;
 
 import javax.inject.Inject;
-import java.util.Optional;
 
 public class CheckoutShippingController extends CartController {
 
 
     private final ShippingMethods shippingMethods;
-    private final CSRF.TokenProvider csrfTokenProvider;
     private final ProductDataConfig productDataConfig;
 
     @Inject
-    public CheckoutShippingController(final ControllerDependency controllerDependency, final ShippingMethods shippingMethods, final CSRF.TokenProvider csrfTokenProvider, final ProductDataConfig productDataConfig) {
+    public CheckoutShippingController(final ControllerDependency controllerDependency, final ShippingMethods shippingMethods, final ProductDataConfig productDataConfig) {
         super(controllerDependency);
         this.shippingMethods = shippingMethods;
-        this.csrfTokenProvider = csrfTokenProvider;
         this.productDataConfig = productDataConfig;
     }
 
+    @AddCSRFToken
     public F.Promise<Result> show(final String languageTag) {
         final UserContext userContext = userContext(languageTag);
         final F.Promise<Cart> cartPromise = getOrCreateCart(userContext, session());
         return cartPromise.map(cart -> {
             final Messages messages = messages(userContext);
-            final CheckoutShippingContent content = new CheckoutShippingContent(cart, messages, configuration(), reverseRouter(), userContext, flash(), csrfTokenProvider.generateToken(), shippingMethods, productDataConfig);
+            final String csrfToken = session("csrfToken");
+            final CheckoutShippingContent content = new CheckoutShippingContent(cart, messages, configuration(), reverseRouter(), userContext, flash(), csrfToken, shippingMethods, productDataConfig);
             final SunrisePageData pageData = pageData(userContext, content);
             return ok(templateService().renderToHtml("checkout-shipping", pageData, userContext.locales()));
         });
@@ -42,8 +41,6 @@ public class CheckoutShippingController extends CartController {
 
     @RequireCSRFCheck
     public F.Promise<Result> process(final String languageTag) {
-        final Optional<CSRF.Token> token = CSRF.getToken(request());
-        System.err.println(token);
         return F.Promise.pure(TODO);
     }
 
