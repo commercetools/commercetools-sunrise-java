6ac33a81a76467f4077e15a6a6d28b58042e7cb5
schleichardt <michael.schleichardt@commercetools.de>
2015-11-17 16:01:24 +0100

    Integrate order summary in checkout shipping. #190

--- purchase/app/purchase/CheckoutShippingContent.java
+++ purchase/app/purchase/CheckoutShippingContent.java
@@ -1,6 +1,7 @@
 package purchase;
 
 import common.contexts.UserContext;
+import common.models.ProductDataConfig;
 import common.pages.PageContent;
 import common.pages.ReverseRouter;
 import io.sphere.sdk.carts.Cart;
@@ -11,16 +12,18 @@ import play.mvc.Http;
 public class CheckoutShippingContent extends PageContent {
     private StepWidgetBean stepWidget;
     private CheckoutShippingFormBean shippingForm;
+    private CartOrderBean cart;
 
 
     public CheckoutShippingContent() {
     }
 
-    public CheckoutShippingContent(final Cart cart, final Messages messages, final Configuration configuration, final ReverseRouter reverseRouter, final UserContext userContext, final Http.Flash flash, final String csrfToken, final ShippingMethods shippingMethods) {
+    public CheckoutShippingContent(final Cart cart, final Messages messages, final Configuration configuration, final ReverseRouter reverseRouter, final UserContext userContext, final Http.Flash flash, final String csrfToken, final ShippingMethods shippingMethods, final ProductDataConfig productDataConfig) {
         final StepWidgetBean stepWidget = new StepWidgetBean();
         stepWidget.setShippingStepActive(true);
         setStepWidget(stepWidget);
         setShippingForm(new CheckoutShippingFormBean(cart, reverseRouter, csrfToken, userContext, shippingMethods, messages, configuration));
+        setCart(new CartOrderBean(cart, userContext, productDataConfig));
     }
 
     public StepWidgetBean getStepWidget() {
@@ -37,6 +40,14 @@ public class CheckoutShippingContent extends PageContent {
 
     public void setShippingForm(final CheckoutShippingFormBean shippingForm) {
         this.shippingForm = shippingForm;
+    }
+
+    public CartOrderBean getCart() {
+        return cart;
+    }
+
+    public void setCart(final CartOrderBean cart) {
+        this.cart = cart;
     }
 
     @Override
--- purchase/app/purchase/CheckoutShippingController.java
+++ purchase/app/purchase/CheckoutShippingController.java
@@ -2,6 +2,7 @@ package purchase;
 
 import common.contexts.UserContext;
 import common.controllers.ControllerDependency;
+import common.models.ProductDataConfig;
 import common.pages.SunrisePageData;
 import io.sphere.sdk.carts.Cart;
 import play.filters.csrf.CSRF;
@@ -18,12 +19,14 @@ public class CheckoutShippingController extends CartController {
 
     private final ShippingMethods shippingMethods;
     private final CSRF.TokenProvider csrfTokenProvider;
+    private final ProductDataConfig productDataConfig;
 
     @Inject
-    public CheckoutShippingController(final ControllerDependency controllerDependency, final ShippingMethods shippingMethods, final CSRF.TokenProvider csrfTokenProvider) {
+    public CheckoutShippingController(final ControllerDependency controllerDependency, final ShippingMethods shippingMethods, final CSRF.TokenProvider csrfTokenProvider, final ProductDataConfig productDataConfig) {
         super(controllerDependency);
         this.shippingMethods = shippingMethods;
         this.csrfTokenProvider = csrfTokenProvider;
+        this.productDataConfig = productDataConfig;
     }
 
     public F.Promise<Result> show(final String languageTag) {
@@ -31,7 +34,7 @@ public class CheckoutShippingController extends CartController {
         final F.Promise<Cart> cartPromise = getOrCreateCart(userContext, session());
         return cartPromise.map(cart -> {
             final Messages messages = messages(userContext);
-            final CheckoutShippingContent content = new CheckoutShippingContent(cart, messages, configuration(), reverseRouter(), userContext, flash(), csrfTokenProvider.generateToken(), shippingMethods);
+            final CheckoutShippingContent content = new CheckoutShippingContent(cart, messages, configuration(), reverseRouter(), userContext, flash(), csrfTokenProvider.generateToken(), shippingMethods, productDataConfig);
             final SunrisePageData pageData = pageData(userContext, content);
             return ok(templateService().renderToHtml("checkout-shipping", pageData, userContext.locales()));
         });
