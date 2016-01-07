5ab4f3566c1e552488e4432c6ce659f9948469f0
Laura Luiz <laura.luiz@commercetools.de>
2016-01-07 16:43:06 +0100

    Accept arguments but do not implement it

--- common/app/common/i18n/I18nResolver.java
+++ common/app/common/i18n/I18nResolver.java
@@ -15,20 +15,22 @@ public interface I18nResolver {
      * @param bundle name of the bundle containing the message key
      * @param key identifier of the message
      * @param locale the locale used to translate the message
-     * @return the resolved message in the given language
+     * @param args optional list of arguments
+     * @return the resolved message in the given language, or absent if it could not be found
      */
-    Optional<String> get(final String bundle, final String key, final Locale locale);
+    Optional<String> get(final String bundle, final String key, final Locale locale, final Object... args);
 
     /**
      * Resolves i18n message identified by a bundle and a key for the the first found given locale.
      * @param bundle name of the bundle containing the message key
      * @param key identifier of the message
      * @param locales the list of locales used to translate the message
-     * @return the resolved message in the first found given language
+     * @param args optional list of arguments
+     * @return the resolved message in the any of the given languages, or absent if it could not be found
      */
-    default Optional<String> get(final String bundle, final String key, final List<Locale> locales) {
+    default Optional<String> get(final String bundle, final String key, final List<Locale> locales, final Object... args) {
         for (final Locale locale : locales) {
-            final Optional<String> message = get(bundle, key, locale);
+            final Optional<String> message = get(bundle, key, locale, args);
             if (message.isPresent()) {
                 return message;
             }
@@ -36,11 +38,27 @@ public interface I18nResolver {
         return Optional.empty();
     }
 
-    default String getOrEmpty(final String bundle, final String key, final Locale locale) {
-        return get(bundle, key, locale).orElse("");
+    /**
+     * Resolves i18n message identified by a bundle and a key for the the first found given locale.
+     * @param bundle name of the bundle containing the message key
+     * @param key identifier of the message
+     * @param locale the locale used to translate the message
+     * @param args optional list of arguments
+     * @return the resolved message in the first found given language, or empty string if it could not be found
+     */
+    default String getOrEmpty(final String bundle, final String key, final Locale locale, final Object... args) {
+        return get(bundle, key, locale, args).orElse("");
     }
 
-    default String getOrEmpty(final String bundle, final String key, final List<Locale> locales) {
-        return get(bundle, key, locales).orElse("");
+    /**
+     * Resolves i18n message identified by a bundle and a key for the the first found given locale.
+     * @param bundle name of the bundle containing the message key
+     * @param key identifier of the message
+     * @param locales the list of locales used to translate the message
+     * @param args optional list of arguments
+     * @return the resolved message in the any of the given languages, or empty string if it could not be found
+     */
+    default String getOrEmpty(final String bundle, final String key, final List<Locale> locales, final Object... args) {
+        return get(bundle, key, locales, args).orElse("");
     }
 }
