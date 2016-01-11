f560aa7329daf3165ed658087846ab24a027b5af
Laura Luiz <laura.luiz@commercetools.de>
2016-01-11 13:53:28 +0100

    Check only for template loaders

--- app/inject/TemplateServiceProvider.java
+++ app/inject/TemplateServiceProvider.java
@@ -37,6 +37,9 @@ class TemplateServiceProvider implements Provider<TemplateService> {
     @Override
     public TemplateService get() {
         final List<TemplateLoader> templateLoaders = initializeTemplateLoaders(CONFIG_TEMPLATE_LOADERS);
+        if (templateLoaders.isEmpty()) {
+            throw new SunriseInitializationException("No Handlebars template loaders found in configuration '" + CONFIG_TEMPLATE_LOADERS + "'");
+        }
         final List<TemplateLoader> fallbackContexts = initializeTemplateLoaders(CONFIG_FALLBACK_CONTEXTS);
         final boolean cacheIsEnabled = configuration.getBoolean(CONFIG_CACHE_ENABLED, false);
         Logger.debug("Provide HandlebarsTemplateService: template loaders [{}], cache enabled {}",
@@ -46,14 +49,10 @@ class TemplateServiceProvider implements Provider<TemplateService> {
     }
 
     private List<TemplateLoader> initializeTemplateLoaders(final String configKey) {
-        final List<TemplateLoader> templateLoaders = configuration.getConfigList(configKey, emptyList())
+        return configuration.getConfigList(configKey, emptyList())
                 .stream()
                 .map(this::initializeTemplateLoader)
                 .collect(toList());
-        if (templateLoaders.isEmpty()) {
-            throw new SunriseInitializationException("No Handlebars template loaders found in configuration '" + CONFIG_TEMPLATE_LOADERS + "'");
-        }
-        return templateLoaders;
     }
 
     private TemplateLoader initializeTemplateLoader(final Configuration loaderConfig) {
