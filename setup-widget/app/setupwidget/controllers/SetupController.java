e20b2b35b7e0a52a74cb1fd1b2aba2212e6c380f
Laura Luiz <laura.luiz@commercetools.de>
2016-01-24 13:35:57 +0100

    Allow to enable/disable metrics to improve performance

--- setup-widget/app/setupwidget/controllers/SetupController.java
+++ setup-widget/app/setupwidget/controllers/SetupController.java
@@ -4,11 +4,9 @@ import play.Logger;
 import setupwidget.models.SphereCredentials;
 import play.Configuration;
 import play.data.Form;
-import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Result;
-import setupwidget.views.html.setup;
-import setupwidget.views.html.success;
+import setupwidget.views.html.*;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -18,33 +16,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.Supplier;
 
-import static play.libs.F.Promise.pure;
-
 @Singleton
 public class SetupController extends Controller {
+    private static final String CONFIG_SETUP_ENABLED = "application.setup.enabled";
     private static final Form<SphereCredentials> SPHERE_CREDENTIALS_FORM = Form.form(SphereCredentials.class);
     private static final Path PATH = FileSystems.getDefault().getPath("conf", "dev.conf");
 
-    private final Configuration config;
+    private final boolean setupEnabled;
     private boolean setupComplete;
 
     @Inject
-    public SetupController(final Configuration config) {
-        this.config = config;
+    public SetupController(final Configuration configuration) {
+        this.setupEnabled = configuration.getBoolean(CONFIG_SETUP_ENABLED, true);
         this.setupComplete = doesConfigFileExist();
     }
 
     public Result handleOrFallback(final Supplier<Result> fallback) {
-        final boolean isWidgetActionsRequired = isSettingsWidgetEnabled() && !isSetupComplete();
-        return isWidgetActionsRequired ? renderForm() : fallback.get();
+        final boolean isSetupRequired = setupEnabled && !isSetupComplete();
+        return isSetupRequired ? renderForm() : fallback.get();
     }
 
     public Result renderForm() {
-        return onWidgetEnabled(() -> ok(setup.render(SPHERE_CREDENTIALS_FORM)));
+        return onSetupEnabled(() -> ok(setup.render(SPHERE_CREDENTIALS_FORM)));
     }
 
     public Result processForm() {
-        return onWidgetEnabled(() -> {
+        return onSetupEnabled(() -> {
             final Form<SphereCredentials> boundForm = SPHERE_CREDENTIALS_FORM.bindFromRequest();
             final Result result;
             if (boundForm.hasErrors()) {
@@ -62,32 +59,28 @@ public class SetupController extends Controller {
         });
     }
 
-    private Result onWidgetEnabled(final Supplier<Result> action) {
-        return isSettingsWidgetEnabled() ? action.get() : notFound();
-    }
-
-    private static void writeSettingsFile(String content) {
-        try {
-            Files.write(PATH, content.getBytes());
-            Logger.info("CTP credentials saved in " + PATH.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private boolean isSettingsWidgetEnabled() {
-        return config.getBoolean("application.settingsWidget.enabled", true);
+    private Result onSetupEnabled(final Supplier<Result> action) {
+        return setupEnabled ? action.get() : notFound();
     }
 
     private boolean isSetupComplete() {
         if (!setupComplete) {
             setupComplete = doesConfigFileExist();
-            Logger.debug("Setup is completed: " + setupComplete);
+            Logger.debug("Setup is complete: " + setupComplete);
         }
         return setupComplete;
     }
 
     private boolean doesConfigFileExist() {
         return Files.exists(PATH);
+    }
+
+    private static void writeSettingsFile(final String content) {
+        try {
+            Files.write(PATH, content.getBytes());
+            Logger.info("CTP credentials saved in " + PATH.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
