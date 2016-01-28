cc656814c3a69b03f0dde4e67564df5aec60503d
Laura Luiz <laura.luiz@commercetools.de>
2016-01-28 12:44:26 +0100

    Update to latest handlebars version with cache improvements and remove fallback context

--- app/inject/TemplateServiceProvider.java
+++ app/inject/TemplateServiceProvider.java
@@ -19,8 +19,6 @@ import static java.util.stream.Collectors.toList;
 
 class TemplateServiceProvider implements Provider<TemplateService> {
     private static final String CONFIG_TEMPLATE_LOADERS = "handlebars.templateLoaders";
-    private static final String CONFIG_FALLBACK_CONTEXTS = "handlebars.fallbackContexts";
-    private static final String CONFIG_CACHE_ENABLED = "handlebars.cache.enabled";
     private static final String CLASSPATH_TYPE = "classpath";
     private static final String FILE_TYPE = "file";
     private static final String TYPE_ATTR = "type";
@@ -40,12 +38,9 @@ class TemplateServiceProvider implements Provider<TemplateService> {
         if (templateLoaders.isEmpty()) {
             throw new SunriseInitializationException("No Handlebars template loaders found in configuration '" + CONFIG_TEMPLATE_LOADERS + "'");
         }
-        final List<TemplateLoader> fallbackContexts = initializeTemplateLoaders(CONFIG_FALLBACK_CONTEXTS);
-        final boolean cacheIsEnabled = configuration.getBoolean(CONFIG_CACHE_ENABLED, false);
-        Logger.debug("Provide HandlebarsTemplateService: template loaders [{}], cache enabled {}",
-                templateLoaders.stream().map(TemplateLoader::getPrefix).collect(joining(", ")),
-                cacheIsEnabled);
-        return HandlebarsTemplateService.of(templateLoaders, fallbackContexts, i18NResolver, cacheIsEnabled);
+        Logger.debug("Provide HandlebarsTemplateService: template loaders [{}]]",
+                templateLoaders.stream().map(TemplateLoader::getPrefix).collect(joining(", ")));
+        return HandlebarsTemplateService.of(templateLoaders, i18NResolver);
     }
 
     private List<TemplateLoader> initializeTemplateLoaders(final String configKey) {
--- common/app/common/templates/HandlebarsTemplateService.java
+++ common/app/common/templates/HandlebarsTemplateService.java
@@ -1,40 +1,31 @@
 package common.templates;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.ValueResolver;
-import com.github.jknack.handlebars.context.JavaBeanValueResolver;
-import com.github.jknack.handlebars.context.MapValueResolver;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.cache.HighConcurrencyTemplateCache;
 import com.github.jknack.handlebars.io.TemplateLoader;
 import common.controllers.PageData;
 import common.i18n.I18nResolver;
 import play.Logger;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
+import java.util.List;
+import java.util.Locale;
 
 import static java.util.stream.Collectors.toList;
 
 public final class HandlebarsTemplateService implements TemplateService {
     private final Handlebars handlebars;
-    private final List<TemplateLoader> fallbackContexts;
-    private final boolean cacheEnabled;
 
-    private HandlebarsTemplateService(final Handlebars handlebars, final List<TemplateLoader> fallbackContexts,
-                                      final boolean cacheEnabled) {
+    private HandlebarsTemplateService(final Handlebars handlebars) {
         this.handlebars = handlebars;
-        this.fallbackContexts = fallbackContexts;
-        this.cacheEnabled = cacheEnabled;
     }
 
     @Override
     public String render(final String templateName, final PageData pageData, final List<Locale> locales) {
         final Template template = compileTemplate(templateName);
-        final Context context = buildContext(pageData, templateName);
+        final Context context = Context.newContext(pageData);;
         context.data("locales", locales.stream().map(Locale::toLanguageTag).collect(toList()));
         try {
             Logger.debug("Rendering template " + templateName);
@@ -44,13 +35,15 @@ public final class HandlebarsTemplateService implements TemplateService {
         }
     }
 
-    public static TemplateService of(final List<TemplateLoader> templateLoaders, final List<TemplateLoader> fallbackContexts,
-                                     final I18nResolver i18NResolver, final boolean cacheEnabled) {
+    public static TemplateService of(final List<TemplateLoader> templateLoaders, final I18nResolver i18NResolver) {
         final TemplateLoader[] loaders = templateLoaders.toArray(new TemplateLoader[templateLoaders.size()]);
-        final Handlebars handlebars = new Handlebars().with(loaders).infiniteLoops(true);
+        final Handlebars handlebars = new Handlebars()
+                .with(loaders)
+                .with(new HighConcurrencyTemplateCache())
+                .infiniteLoops(true);
         handlebars.registerHelper("i18n", new CustomI18nHelper(i18NResolver));
         handlebars.registerHelper("json", new HandlebarsJsonHelper<>());
-        return new HandlebarsTemplateService(handlebars, fallbackContexts, cacheEnabled);
+        return new HandlebarsTemplateService(handlebars);
     }
 
     private Template compileTemplate(final String templateName) {
@@ -59,43 +52,5 @@ public final class HandlebarsTemplateService implements TemplateService {
         } catch (IOException e) {
             throw new TemplateNotFoundException("Could not find the default template", e);
         }
-    }
-
-    private Context buildContext(final PageData pageData, final String templateName) {
-        Context.Builder builder = Context.newBuilder(pageData)
-                .resolver(valueResolver(), MapValueResolver.INSTANCE);
-        for (final TemplateLoader fallbackContext : fallbackContexts) {
-            final Optional<Map<String, ?>> map = buildFallbackContext(fallbackContext, templateName);
-            if (map.isPresent()) {
-                builder = builder.combine(map.get());
-            }
-        }
-        return builder.build();
-    }
-
-    private ValueResolver valueResolver() {
-        return cacheEnabled ? JavaBeanValueResolver.INSTANCE : NonCachedJavaBeanValueResolver.INSTANCE;
-    }
-
-    private Optional<Map<String, ?>> buildFallbackContext(final TemplateLoader fallbackLoader, final String templateName) {
-        return getResource(fallbackLoader, templateName).map(stream -> {
-            try {
-                return new ObjectMapper().readValue(stream, HashMap.class);
-            } catch (IOException e) {
-                Logger.error("Could not read fallback context", e);
-                return null;
-            }
-        });
-    }
-
-    private Optional<InputStream> getResource(final TemplateLoader fallbackLoader, final String templateName) {
-        final InputStream resource;
-        if (fallbackLoader instanceof ClassPathTemplateLoader) {
-            final String path = fallbackLoader.resolve(templateName).replaceAll("^/?(.*)\\.hbs$", "$1.json");
-            resource = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-        } else {
-            throw new RuntimeException("Invalid fallback context type, only classpath supported");
-        }
-        return Optional.ofNullable(resource);
     }
 }
