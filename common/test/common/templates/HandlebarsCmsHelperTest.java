4de4d12a16e0328d44174e5ed001c157ed45a80f
Laura Luiz <laura.luiz@commercetools.de>
2016-03-12 01:48:20 +0100

    Test cms handlebars helper

--- common/test/common/templates/HandlebarsCmsHelperTest.java
+++ common/test/common/templates/HandlebarsCmsHelperTest.java
@@ -2,6 +2,9 @@ package common.templates;
 
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
+import common.cms.CmsPage;
+import common.cms.CmsService;
+import common.cms.local.LocalCmsService;
 import common.controllers.PageData;
 import common.i18n.I18nResolver;
 import common.i18n.TestableI18nResolver;
@@ -19,20 +22,13 @@ import static java.util.Locale.ENGLISH;
 import static java.util.Locale.GERMAN;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class HandlebarsI18NHelperTest {
-    private static final TemplateLoader DEFAULT_LOADER = new ClassPathTemplateLoader("/templates/i18nHelper");
+public class HandlebarsCmsHelperTest {
+    private static final TemplateLoader DEFAULT_LOADER = new ClassPathTemplateLoader("/templates/cmsHelper");
     private static final PageData SOME_PAGE_DATA = new TestablePageData();
 
     @Test
     public void resolvesMessage() throws Exception {
-        final Map<String, String> i18nMap = new HashMap<>();
-        i18nMap.put("en/main:foo", "bar");
-        testTemplate("simple", ENGLISH, i18nMap, html -> assertThat(html).contains("bar"));
-    }
-
-    @Test
-    public void resolvesMessageWithBundle() throws Exception {
-        testTemplate("bundle", ENGLISH, defaultI18nMap(), html -> assertThat(html).contains("something"));
+        testTemplate("simple", ENGLISH, defaultI18nMap(), html -> assertThat(html).contains("something"));
     }
 
     @Test
@@ -42,17 +38,22 @@ public class HandlebarsI18NHelperTest {
 
     @Test
     public void languageNotFound() throws Exception {
-        testTemplate("bundle", GERMAN, defaultI18nMap(), html -> assertThat(html).isEmpty());
+        testTemplate("simple", GERMAN, defaultI18nMap(), html -> assertThat(html).isEmpty());
     }
 
     @Test
-    public void keyNotFound() throws Exception {
-        testTemplate("missingKey", ENGLISH, defaultI18nMap(), html -> assertThat(html).isEmpty());
+    public void contentTypeNotFound() throws Exception {
+        testTemplate("missingType", ENGLISH, defaultI18nMap(), html -> assertThat(html).isEmpty());
     }
 
     @Test
-    public void bundleNotFound() throws Exception {
-        testTemplate("missingBundle", ENGLISH, defaultI18nMap(), html -> assertThat(html).isEmpty());
+    public void contentIdNotFound() throws Exception {
+        testTemplate("missingId", ENGLISH, defaultI18nMap(), html -> assertThat(html).isEmpty());
+    }
+
+    @Test
+    public void contentFieldNotFound() throws Exception {
+        testTemplate("missingField", ENGLISH, defaultI18nMap(), html -> assertThat(html).isEmpty());
     }
 
     private static void testTemplate(final String templateName, final Locale locale, final Map<String, String> i18nMap,
@@ -68,7 +69,9 @@ public class HandlebarsI18NHelperTest {
     }
 
     private static String renderTemplate(final String templateName, final TemplateService templateService, final Locale locale) {
-        return templateService.render(templateName, SOME_PAGE_DATA, singletonList(locale));
+        final CmsService cmsService =  LocalCmsService.of(i18nResolver(defaultI18nMap()));
+        final CmsPage cmsPage = cmsService.getPage(singletonList(locale), "cms").get(0);
+        return templateService.render(templateName, SOME_PAGE_DATA, singletonList(locale), cmsPage);
     }
 
     private static I18nResolver i18nResolver(final Map<String, String> i18nMap) {
@@ -77,8 +80,8 @@ public class HandlebarsI18NHelperTest {
 
     private static Map<String, String> defaultI18nMap() {
         final Map<String, String> i18nMap = new HashMap<>();
-        i18nMap.put("en/somebundle:somekey", "something");
-        i18nMap.put("es/somebundle:somekey", "algo");
+        i18nMap.put("en/sometype:someid.somefield", "something");
+        i18nMap.put("es/sometype:someid.somefield", "algo");
         return i18nMap;
     }
 }
