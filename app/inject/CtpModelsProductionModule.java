c6725272bf22d17a5fff17e6171f04123073093e
Laura Luiz <laura.luiz@commercetools.de>
2016-04-12 17:51:51 +0200

    Split providers into different selectors config

--- app/inject/CtpModelsProductionModule.java
+++ app/inject/CtpModelsProductionModule.java
@@ -3,7 +3,6 @@ package inject;
 import com.google.inject.AbstractModule;
 import common.contexts.ProjectContext;
 import common.models.ProductDataConfig;
-import productcatalog.productoverview.search.SearchConfig;
 import productcatalog.services.ProductService;
 import shoppingcart.checkout.shipping.ShippingMethods;
 
@@ -18,7 +17,6 @@ public class CtpModelsProductionModule extends AbstractModule {
     protected void configure() {
         bind(ProjectContext.class).toProvider(ProjectContextProvider.class).in(Singleton.class);
         bind(ProductDataConfig.class).toProvider(ProductDataConfigProvider.class).in(Singleton.class);
-        bind(SearchConfig.class).toProvider(SearchConfigProvider.class).in(Singleton.class);
         bind(ShippingMethods.class).toProvider(ShippingMethodsProvider.class).in(Singleton.class);
         bind(ProductService.class).toProvider(ProductServiceProvider.class);
     }
