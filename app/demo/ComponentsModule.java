d885d38dff6d82628846ce8b7a23d6cea4548aca
schleichardt <michael.schleichardt@commercetools.de>
2016-06-20 16:22:15 +0200

    provide workaround for "Operator failed: javax.money.DefaultMonetaryRoundingsSingletonSpi$DefaultCurrencyRounding", close #404

--- app/demo/ComponentsModule.java
+++ app/demo/ComponentsModule.java
@@ -1,18 +1,29 @@
 package demo;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
+import com.commercetools.sunrise.common.localization.LocationSelectorControllerComponent;
+import com.commercetools.sunrise.common.pages.DefaultPageNavMenuControllerComponent;
 import com.commercetools.sunrise.framework.MultiControllerComponentResolver;
 import com.commercetools.sunrise.framework.MultiControllerComponentResolverBuilder;
-import com.commercetools.sunrise.shoppingcart.checkout.CheckoutCommonComponent;
-import com.commercetools.sunrise.common.pages.DefaultPageNavMenuControllerComponent;
-import com.commercetools.sunrise.common.localization.LocationSelectorControllerComponent;
 import com.commercetools.sunrise.shoppingcart.MiniCartControllerComponent;
+import com.commercetools.sunrise.shoppingcart.checkout.CheckoutCommonComponent;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import io.sphere.sdk.utils.MoneyImpl;
+
+import javax.money.Monetary;
+import javax.money.MonetaryRounding;
 
 public class ComponentsModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        applyJavaMoneyHack();
+    }
+
+    private void applyJavaMoneyHack() {
+        //fixes https://github.com/commercetools/commercetools-sunrise-java/issues/404
+        //exception play.api.http.HttpErrorHandlerExceptions$$anon$1: Execution exception[[CompletionException: java.lang.IllegalArgumentException: java.util.concurrent.CompletionException: io.sphere.sdk.json.JsonException: detailMessage: com.fasterxml.jackson.databind.JsonMappingException: Operator failed: javax.money.DefaultMonetaryRoundingsSingletonSpi$DefaultCurrencyRounding@1655879e (through reference chain: io.sphere.sdk.payments.PaymentDraftImpl["amountPlanned"])
+        Monetary.getDefaultRounding().apply(MoneyImpl.ofCents(123, "EUR"));
     }
 
     @Provides
