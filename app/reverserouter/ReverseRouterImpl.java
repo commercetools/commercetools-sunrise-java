24a6c02125729edebd7ba6f92fd926b31d4621a3
schleichardt <michael.schleichardt@commercetools.de>
2016-06-22 09:49:55 +0200

    wip checkout thankyou controller as framework controller

--- app/reverserouter/ReverseRouterImpl.java
+++ app/reverserouter/ReverseRouterImpl.java
@@ -14,7 +14,6 @@ import static demo.productcatalog.routes.*;
 import static demo.myaccount.routes.*;
 import static demo.shoppingcart.routes.*;
 import static com.commercetools.sunrise.shoppingcart.cartdetail.routes.*;
-import static com.commercetools.sunrise.shoppingcart.checkout.thankyou.routes.*;
 import static setupwidget.controllers.routes.*;
 
 public class ReverseRouterImpl extends Base implements ReverseRouter, HomeReverseRouter, ProductReverseRouter, CheckoutReverseRouter, AddressBookReverseRouter, SetupReverseRouter {
---
+++ shopping-cart/app/com/commercetools/sunrise/shoppingcart/checkout/thankyou/SunriseCheckoutThankYouPageController.java
@@ -0,0 +1,78 @@
+package com.commercetools.sunrise.shoppingcart.checkout.thankyou;
+
+import com.commercetools.sunrise.common.contexts.UserContext;
+import com.commercetools.sunrise.common.controllers.WithOverwriteableTemplateName;
+import com.commercetools.sunrise.common.reverserouter.HomeReverseRouter;
+import com.commercetools.sunrise.common.template.i18n.I18nIdentifier;
+import com.commercetools.sunrise.common.template.i18n.I18nResolver;
+import com.commercetools.sunrise.shoppingcart.OrderSessionUtils;
+import com.commercetools.sunrise.shoppingcart.common.SunriseFrameworkCartController;
+import io.sphere.sdk.orders.Order;
+import io.sphere.sdk.orders.queries.OrderByIdGet;
+import play.libs.concurrent.HttpExecution;
+import play.mvc.Call;
+import play.mvc.Result;
+import play.twirl.api.Html;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.Arrays.asList;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+@Singleton
+public abstract class SunriseCheckoutThankYouPageController extends SunriseFrameworkCartController implements WithOverwriteableTemplateName {
+
+    @Inject
+    private I18nResolver i18nResolver;
+    @Inject
+    private HomeReverseRouter homeReverseRouter;
+
+    public CompletionStage<Result> show(final String languageTag) {
+        return doRequest(() -> {
+            return OrderSessionUtils.getLastOrderId(session())
+                    .map(lastOrderId -> findOrder(lastOrderId)
+                            .thenComposeAsync(orderOpt -> orderOpt
+                                    .map(order -> handleFoundOrder(order, userContext()))
+                                    .orElseGet(() -> handleNotFoundOrder(userContext())),
+                                    HttpExecution.defaultContext())
+                    )
+                    .orElseGet(() -> handleNotFoundOrder(userContext()));
+        });
+    }
+
+    protected CompletionStage<Optional<Order>> findOrder(final String lastOrderId) {
+        return sphere().execute(OrderByIdGet.of(lastOrderId))
+                .thenApplyAsync(Optional::ofNullable, HttpExecution.defaultContext());
+    }
+
+    protected CompletionStage<Result> handleFoundOrder(final Order order, final UserContext userContext) {
+        final CheckoutThankYouPageContent pageContent = new CheckoutThankYouPageContent();
+        pageContent.setOrder(createCartLikeBean(order, userContext));
+        return asyncOk(renderCheckoutThankYouPage(pageContent, userContext));
+    }
+
+    protected CompletionStage<Result> handleNotFoundOrder(final UserContext userContext) {
+        final Call call = homeReverseRouter.homePageCall(userContext.locale().toLanguageTag());
+        return completedFuture(redirect(call));
+    }
+
+    protected CompletionStage<Html> renderCheckoutThankYouPage(final CheckoutThankYouPageContent pageContent, final UserContext userContext) {
+        pageContent.setTitle(i18nResolver.getOrEmpty(userContext.locales(), I18nIdentifier.of("checkout:thankYouPage.title")));
+        return renderPage(pageContent, getTemplateName());
+    }
+
+    @Override
+    public String getTemplateName() {
+        return "checkout-thankyou";
+    }
+
+    @Override
+    public Set<String> getFrameworkTags() {
+        return new HashSet<>(asList("checkout", "checkout-thankyou"));
+    }
+}
