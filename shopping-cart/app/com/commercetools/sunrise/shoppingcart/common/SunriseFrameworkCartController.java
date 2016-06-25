0a70c2d1f53b6c304cd7cd787c23a4c40792e6e7
schleichardt <michael.schleichardt@commercetools.de>
2016-06-25 10:14:04 +0200

    fix getOrCreateCart

--- shopping-cart/app/com/commercetools/sunrise/shoppingcart/common/SunriseFrameworkCartController.java
+++ shopping-cart/app/com/commercetools/sunrise/shoppingcart/common/SunriseFrameworkCartController.java
@@ -23,7 +23,6 @@ import io.sphere.sdk.carts.queries.CartQueryBuilder;
 import io.sphere.sdk.models.Address;
 import io.sphere.sdk.shippingmethods.ShippingMethod;
 import io.sphere.sdk.shippingmethods.queries.ShippingMethodsByCartGet;
-import play.libs.concurrent.HttpExecution;
 import play.mvc.Http;
 
 import java.util.List;
@@ -33,6 +32,7 @@ import java.util.concurrent.CompletionStage;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.CompletableFuture.completedFuture;
+import static play.libs.concurrent.HttpExecution.defaultContext;
 
 @NoCache
 public abstract class SunriseFrameworkCartController extends SunriseFrameworkController {
@@ -42,16 +42,15 @@ public abstract class SunriseFrameworkCartController extends SunriseFrameworkCon
     @Inject
     protected CartLikeBeanFactory cartLikeBeanFactory;
 
-    protected CompletionStage<Cart> getOrCreateCart(final UserContext userContext, final Http.Session session) {
-        final CompletionStage<Cart> cartFuture = fetchCart(userContext, session)
-                .thenComposeAsync(cart -> updateCartWithUserPreferences(cart, userContext), HttpExecution.defaultContext());
-        cartFuture.thenAcceptAsync(cart ->
-                CartSessionUtils.overwriteCartSessionData(cart, session, userContext, productReverseRouter), HttpExecution.defaultContext());
-        return cartFuture;
-    }
-
     protected CompletionStage<Cart> getOrCreateCart() {
-        return getOrCreateCart(userContext(), session())
+        final UserContext userContext = userContext();
+        final Http.Session session = session();
+        return fetchCart(userContext, session)
+                .thenComposeAsync(cart -> updateCartWithUserPreferences(cart, userContext), defaultContext())
+                .thenApply(cart -> {
+                    CartSessionUtils.overwriteCartSessionData(cart, session, userContext, productReverseRouter);
+                    return cart;
+                })
                 .thenApply(cart -> {
                     hooks().runAsyncHook(CartLoadedHook.class, hook -> hook.cartLoaded(cart));
                     return cart;
@@ -106,7 +105,7 @@ public abstract class SunriseFrameworkCartController extends SunriseFrameworkCon
         return sphere().execute(query).thenComposeAsync(carts -> carts.head()
                 .map(cart -> (CompletionStage<Cart>) completedFuture(cart))
                 .orElseGet(() -> createCart(userContext)),
-                HttpExecution.defaultContext());
+                defaultContext());
     }
 
     protected CompletionStage<Cart> updateCartWithUserPreferences(final Cart cart, final UserContext userContext) {
