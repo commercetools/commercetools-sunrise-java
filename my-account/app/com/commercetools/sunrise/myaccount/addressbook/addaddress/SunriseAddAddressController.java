371c090c7738ca8fdc082bc94eb0ef5ca5ec408d
Laura Luiz <laura.luiz@commercetools.de>
2016-06-27 18:00:51 +0200

    Add address with default shipping/billing

--- my-account/app/com/commercetools/sunrise/myaccount/addressbook/addaddress/SunriseAddAddressController.java
+++ my-account/app/com/commercetools/sunrise/myaccount/addressbook/addaddress/SunriseAddAddressController.java
@@ -7,9 +7,12 @@ import com.commercetools.sunrise.myaccount.addressbook.AddressBookManagementCont
 import com.commercetools.sunrise.myaccount.addressbook.AddressFormData;
 import com.commercetools.sunrise.myaccount.addressbook.DefaultAddressFormData;
 import com.google.inject.Injector;
+import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.customers.Customer;
 import io.sphere.sdk.customers.commands.CustomerUpdateCommand;
 import io.sphere.sdk.customers.commands.updateactions.AddAddress;
+import io.sphere.sdk.customers.commands.updateactions.SetDefaultBillingAddress;
+import io.sphere.sdk.customers.commands.updateactions.SetDefaultShippingAddress;
 import io.sphere.sdk.models.Address;
 import io.sphere.sdk.models.SphereException;
 import org.slf4j.Logger;
@@ -24,14 +27,13 @@ import play.twirl.api.Html;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.CompletionStage;
 
 import static io.sphere.sdk.utils.FutureUtils.exceptionallyCompletedFuture;
 import static io.sphere.sdk.utils.FutureUtils.recoverWithAsync;
 import static java.util.Arrays.asList;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 
 @RequestScoped
 public abstract class SunriseAddAddressController extends AddressBookManagementController implements WithOverwriteableTemplateName {
@@ -60,7 +62,7 @@ public abstract class SunriseAddAddressController extends AddressBookManagementC
         return doRequest(() -> {
             logger.debug("show new address form for address in locale={}", languageTag);
             return injector.getInstance(AddAddressActionDataDefaultProvider.class).getActionData(session(), null)
-                    .thenComposeAsync(this::showAddAddress, HttpExecution.defaultContext());
+                    .thenComposeAsync(this::show, HttpExecution.defaultContext());
         });
     }
 
@@ -70,20 +72,15 @@ public abstract class SunriseAddAddressController extends AddressBookManagementC
             logger.debug("try to add address with in locale={}", languageTag);
             final Form<DefaultAddressFormData> form = formFactory.form(DefaultAddressFormData.class).bindFromRequest();
             return injector.getInstance(AddAddressActionDataDefaultProvider.class).getActionData(session(), form)
-                    .thenComposeAsync(this::processAddAddress, HttpExecution.defaultContext());
+                    .thenComposeAsync(this::process, HttpExecution.defaultContext());
         });
     }
 
-    protected <T extends AddressFormData> CompletionStage<Result> showAddAddress(final AddressActionData<T> data) {
+    protected <T extends AddressFormData> CompletionStage<Result> show(final AddressActionData<T> data) {
         return ifNotNullCustomer(data.getCustomer().orElse(null), this::showEmptyForm);
     }
 
-    protected CompletionStage<Result> showEmptyForm(final Customer customer) {
-        final Form<?> form = obtainFilledForm(null);
-        return asyncOk(renderPage(customer, form));
-    }
-
-    protected <T extends AddressFormData> CompletionStage<Result> processAddAddress(final AddressActionData<T> data) {
+    protected <T extends AddressFormData> CompletionStage<Result> process(final AddressActionData<T> data) {
         return ifNotNullCustomer(data.getCustomer().orElse(null), customer -> data.getForm()
                 .map(form -> {
                     if (!form.hasErrors()) {
@@ -93,6 +90,11 @@ public abstract class SunriseAddAddressController extends AddressBookManagementC
                     }
                 }).orElseGet(() -> showEmptyForm(customer))
         );
+    }
+
+    protected CompletionStage<Result> showEmptyForm(final Customer customer) {
+        final Form<?> form = obtainFilledForm(null);
+        return asyncOk(renderPage(customer, form));
     }
 
     protected <T extends AddressFormData> CompletionStage<Result> applySubmittedAddress(final Customer customer, final T formData) {
@@ -121,15 +123,14 @@ public abstract class SunriseAddAddressController extends AddressBookManagementC
     }
 
     protected <T extends AddressFormData> CompletionStage<Customer> addAddressToCustomer(final Customer customer, final T formData) {
-        final AddAddress addAddressAction = AddAddress.of(formData.extractAddress());
-        return sphere().execute(CustomerUpdateCommand.of(customer, addAddressAction));
-//                .thenComposeAsync(updatedCustomer -> {
-//                    findAddress(customer, )
-//                    final List<Customer> updateActions = new ArrayList<>();
-//                    if (formData.isDefaultShippingAddress()) {
-//                        updateActions.add(SetDefaultShippingAddress.of())
-//                    }
-//                });
+        final Address address = formData.extractAddress();
+        return addAddress(customer, address)
+                .thenComposeAsync(updatedCustomer -> findAddress(updatedCustomer, address)
+                        .map(addressWithId -> {
+                            final boolean defaultShipping = formData.isDefaultShippingAddress();
+                            final boolean defaultBilling = formData.isDefaultBillingAddress();
+                            return setAddressAsDefault(updatedCustomer, addressWithId, defaultShipping, defaultBilling);
+                        }).orElseGet(() -> completedFuture(updatedCustomer)));
     }
 
     protected CompletionStage<Html> renderPage(final Customer customer, final Form<?> form) {
@@ -143,9 +144,30 @@ public abstract class SunriseAddAddressController extends AddressBookManagementC
         return formFactory.form(DefaultAddressFormData.class).fill(formData);
     }
 
-    private Optional<Address> findAddress(final Customer customer, final String addressId) {
+    private CompletionStage<Customer> addAddress(final Customer customer, final Address address) {
+        final AddAddress addAddressAction = AddAddress.of(address);
+        return sphere().execute(CustomerUpdateCommand.of(customer, addAddressAction));
+    }
+
+    private CompletionStage<Customer> setAddressAsDefault(final Customer customer, final Address address,
+                                                          final boolean defaultShipping, final boolean defaultBilling) {
+        final List<UpdateAction<Customer>> updateActions = new ArrayList<>();
+        if (defaultShipping) {
+            updateActions.add(SetDefaultShippingAddress.ofAddress(address));
+        }
+        if (defaultBilling) {
+            updateActions.add(SetDefaultBillingAddress.ofAddress(address));
+        }
+        if (!updateActions.isEmpty()) {
+            return sphere().execute(CustomerUpdateCommand.of(customer, updateActions));
+        } else {
+            return completedFuture(customer);
+        }
+    }
+
+    private Optional<Address> findAddress(final Customer customer, final Address address) {
         return customer.getAddresses().stream()
-                .filter(a -> Objects.equals(a.getId(), addressId))
+                .filter(a -> a.equalsIgnoreId(address))
                 .findFirst();
     }
 }
