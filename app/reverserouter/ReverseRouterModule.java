692d0985145cb2739915cf50b4a9430c870c6acf
schleichardt <michael.schleichardt@commercetools.de>
2016-07-01 15:25:09 +0200

    fix reveres router

--- app/reverserouter/ReverseRouterModule.java
+++ app/reverserouter/ReverseRouterModule.java
@@ -16,6 +16,7 @@ public class ReverseRouterModule extends AbstractModule {
         bind(HomeReverseRouter.class).toInstance(reverseRouter);
         bind(AddressBookReverseRouter.class).toInstance(reverseRouter);
         bind(MyPersonalDetailsReverseRouter.class).toInstance(reverseRouter);
+        bind(MyOrdersReverseRouter.class).toInstance(reverseRouter);
         bind(SetupReverseRouter.class).toInstance(reverseRouter);
     }
 }
