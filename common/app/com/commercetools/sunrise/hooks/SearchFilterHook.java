3c278d654963800e45435d9269d8c0facd2ce358
Laura Luiz <laura.luiz@commercetools.de>
2016-07-04 17:52:58 +0200

    Remove unused hook (replaced by QueryFilterHook)

--- common/app/com/commercetools/sunrise/hooks/SearchFilterHook.java
+++
@@ -1,6 +0,0 @@
-package com.commercetools.sunrise.hooks;
-
-public interface SearchFilterHook<T> extends Hook {
-
-    T filterQuery(T query);
-}
--- common/app/com/commercetools/sunrise/hooks/SingleProductProjectionHook.java
+++ common/app/com/commercetools/sunrise/hooks/SingleProductProjectionHook.java
@@ -1,6 +1,5 @@
 package com.commercetools.sunrise.hooks;
 
-import com.commercetools.sunrise.hooks.Hook;
 import io.sphere.sdk.products.ProductProjection;
 
 import java.util.concurrent.CompletionStage;
