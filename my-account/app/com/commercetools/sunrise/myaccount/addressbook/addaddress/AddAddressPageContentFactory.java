fbd1bea6b4858ba2f23a863e1b34bf9bc3521b5d
schleichardt <michael.schleichardt@commercetools.de>
2016-07-14 09:37:10 +0200

    adjust AddAddressPageContentFactory #416

--- my-account/app/com/commercetools/sunrise/myaccount/addressbook/addaddress/AddAddressPageContentFactory.java
+++ my-account/app/com/commercetools/sunrise/myaccount/addressbook/addaddress/AddAddressPageContentFactory.java
@@ -1,6 +1,5 @@
 package com.commercetools.sunrise.myaccount.addressbook.addaddress;
 
-import com.commercetools.sunrise.common.controllers.WithOverridablePageContent;
 import com.commercetools.sunrise.myaccount.addressbook.AddressFormSettingsFactory;
 import io.sphere.sdk.customers.Customer;
 import io.sphere.sdk.models.Base;
@@ -8,24 +7,18 @@ import play.data.Form;
 
 import javax.inject.Inject;
 
-public class AddAddressPageContentFactory extends Base implements WithOverridablePageContent<AddAddressPageContent> {
+public class AddAddressPageContentFactory extends Base {
 
     @Inject
     private AddressFormSettingsFactory addressFormSettingsFactory;
 
     public AddAddressPageContent create(final Form<?> form, final Customer customer) {
-        final AddAddressPageContent content = createPageContent();
-        fillNewAddressForm(content, form);
-        return content;
+        return fillBean(new AddAddressPageContent(), form, customer);
     }
 
-    protected void fillNewAddressForm(final AddAddressPageContent content, final Form<?> form) {
+    protected <T extends AddAddressPageContent> T fillBean(final T content, final Form<?> form, final Customer customer) {
         content.setNewAddressForm(form);
         content.setNewAddressFormSettings(addressFormSettingsFactory.create(form));
-    }
-
-    @Override
-    public AddAddressPageContent createPageContent() {
-        return new AddAddressPageContent();
+        return content;
     }
 }
