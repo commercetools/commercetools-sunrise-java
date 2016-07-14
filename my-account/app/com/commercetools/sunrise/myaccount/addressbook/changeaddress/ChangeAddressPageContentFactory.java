3f6250add184f0eafc335950543a35e450bf8604
schleichardt <michael.schleichardt@commercetools.de>
2016-07-14 10:13:15 +0200

    adjust ChangeAddressPageContentFactory #416

--- my-account/app/com/commercetools/sunrise/myaccount/addressbook/changeaddress/ChangeAddressPageContentFactory.java
+++ my-account/app/com/commercetools/sunrise/myaccount/addressbook/changeaddress/ChangeAddressPageContentFactory.java
@@ -2,17 +2,21 @@ package com.commercetools.sunrise.myaccount.addressbook.changeaddress;
 
 import com.commercetools.sunrise.myaccount.addressbook.AddressFormSettingsFactory;
 import io.sphere.sdk.customers.Customer;
+import io.sphere.sdk.models.Base;
 import play.data.Form;
 
 import javax.inject.Inject;
 
-public class ChangeAddressPageContentFactory {
+public class ChangeAddressPageContentFactory extends Base {
 
     @Inject
     private AddressFormSettingsFactory addressFormSettingsFactory;
 
     public ChangeAddressPageContent create(final Form<?> form, final Customer customer) {
-        final ChangeAddressPageContent content = new ChangeAddressPageContent();
+        return fillBean(new ChangeAddressPageContent(), form, customer);
+    }
+
+    protected <T extends ChangeAddressPageContent> T fillBean(final T content, final Form<?> form, final Customer customer) {
         fillEditAddressForm(content, form);
         return content;
     }
