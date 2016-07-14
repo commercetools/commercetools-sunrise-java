12e6d0016ac7a5e46f8d5ee3a42e85272a65d089
schleichardt <michael.schleichardt@commercetools.de>
2016-07-14 09:54:11 +0200

    adjust AuthenticationPageContentFactory #416

--- my-account/app/com/commercetools/sunrise/myaccount/authentication/AuthenticationPageContentFactory.java
+++ my-account/app/com/commercetools/sunrise/myaccount/authentication/AuthenticationPageContentFactory.java
@@ -2,12 +2,13 @@ package com.commercetools.sunrise.myaccount.authentication;
 
 import com.commercetools.sunrise.common.forms.TitleFormFieldBeanFactory;
 import com.commercetools.sunrise.myaccount.authentication.signup.SignUpFormSettingsBean;
+import io.sphere.sdk.models.Base;
 import play.data.Form;
 import play.data.FormFactory;
 
 import javax.inject.Inject;
 
-public class AuthenticationPageContentFactory {
+public class AuthenticationPageContentFactory extends Base {
 
     @Inject
     private TitleFormFieldBeanFactory titleFormFieldBeanFactory;
@@ -15,14 +16,21 @@ public class AuthenticationPageContentFactory {
     private FormFactory formFactory;
 
     public AuthenticationPageContent createWithSignUpForm(final Form<?> signUpForm) {
-        final AuthenticationPageContent pageContent = new AuthenticationPageContent();
-        fillSignUpForm(pageContent, signUpForm);
+        return fillBeanWithSignUpForm(new AuthenticationPageContent(), signUpForm);
+    }
+
+    protected <T extends AuthenticationPageContent> T fillBeanWithSignUpForm(final T pageContent, final Form<?> signUpForm) {
         fillLogInForm(pageContent, formFactory.form());
+        fillSignUpForm(pageContent, signUpForm);
         return pageContent;
     }
 
     public AuthenticationPageContent createWithLogInForm(final Form<?> logInForm) {
         final AuthenticationPageContent pageContent = new AuthenticationPageContent();
+        return fillBeanWithLogInForm(pageContent, logInForm);
+    }
+
+    protected <T extends AuthenticationPageContent> T fillBeanWithLogInForm(final T pageContent, final Form<?> logInForm) {
         fillSignUpForm(pageContent, formFactory.form());
         fillLogInForm(pageContent, logInForm);
         return pageContent;
