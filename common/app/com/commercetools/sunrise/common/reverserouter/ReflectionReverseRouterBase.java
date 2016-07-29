030e41564aaea4f203b65b202718e98488f5ce3c
schleichardt <michael.schleichardt@commercetools.de>
2016-07-29 16:58:58 +0200

    fallback to / if route cannot be found

--- common/app/com/commercetools/sunrise/common/reverserouter/ReflectionReverseRouterBase.java
+++ common/app/com/commercetools/sunrise/common/reverserouter/ReflectionReverseRouterBase.java
@@ -6,12 +6,15 @@ import com.commercetools.sunrise.framework.annotations.ReverseRoute;
 import io.sphere.sdk.models.Base;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.concurrent.CompletionException;
+
+import static java.lang.String.format;
 
 abstract class ReflectionReverseRouterBase extends Base {
 
@@ -23,9 +26,12 @@ abstract class ReflectionReverseRouterBase extends Base {
                     .map((parsedRoute) -> getParsedRouteRFunction(parsedRoute, tag))
                     .filter(Optional::isPresent)
                     .map(x -> x.get())
-                    .map(p -> new ReverseCaller(p.getRight(), p.getLeft()))
+                    .map(p -> (ReverseCaller) new ReflectionReverseCaller(p.getRight(), p.getLeft()))
                     .findFirst()
-                    .orElseThrow(() -> new RuntimeException("cannot find method for tag " + tag));
+                    .orElseGet(() -> {
+                        LoggerFactory.getLogger(this.getClass()).warn(format("Cannot find route for %s, falling back to GET /.", tag));
+                        return RootReverseCaller.INSTANCE;
+                    });
             return reverseCaller;
         } catch (final Exception e) {
             throw new RuntimeException(e);
@@ -34,37 +40,36 @@ abstract class ReflectionReverseRouterBase extends Base {
 
     private Optional<Pair<Object, Method>> getParsedRouteRFunction(final ParsedRoute parsedRoute, final String tag) {
         final Class<?> controllerClass = parsedRoute.getControllerClass();
-        Optional<Method> controllerMethodOptional = foo(controllerClass, tag);
-        return controllerMethodOptional.map(m -> createPair(m, controllerClass));
+        Optional<Method> controllerMethodOptional = findControllerMethod(controllerClass, tag);
+        return controllerMethodOptional.flatMap(m -> createPair(m, controllerClass));
     }
 
-    private Pair<Object, Method> createPair(final Method controllerMethod, final Class<?> controllerClass) {
+    private Optional<Pair<Object, Method>> createPair(final Method controllerMethod, final Class<?> controllerClass) {
         try {
             final String packageName = controllerClass.getPackage().getName();
             final Class<?> reverseRouter = Class.forName(packageName + ".routes");
             final Field field = reverseRouter.getField(controllerClass.getSimpleName());
             final Object o = field.get(null);
             final Method reverseRouteMethod = o.getClass().getMethod(controllerMethod.getName(), controllerMethod.getParameterTypes());
-            return ImmutablePair.of(o, reverseRouteMethod);
-        } catch (Exception e) {
+            return Optional.ofNullable(ImmutablePair.of(o, reverseRouteMethod));
+        } catch (final NoSuchMethodException e) {
+            return Optional.empty();
+        } catch (final Exception e) {
             throw new CompletionException(e);
         }
     }
 
-    private Optional<Method> foo(final Class<?> controllerClass, final String tag) {
+    private Optional<Method> findControllerMethod(final Class<?> controllerClass, final String tag) {
         try {
             return Arrays.stream(controllerClass.getMethods())
                     .filter(method -> methodFilter(method, tag)).findFirst();
         } catch (final Exception e) {
-            throw new CompletionException("failed in foo", e);
+            throw new CompletionException("failed in findControllerMethod", e);
         }
     }
 
     private boolean methodFilter(final Method method, final String tag) {
         try {
-            if (method.getName().equals("changeLanguage")) {
-                System.err.println("here");
-            }
             final ReverseRoute annotationsByTypeOption = method.getDeclaredAnnotation(ReverseRoute.class);
             return Optional.ofNullable(annotationsByTypeOption).map(annotationsByType -> Arrays.stream(annotationsByType.value())
                     .anyMatch(value -> tag.equals(value))).orElse(false);
--- common/app/com/commercetools/sunrise/common/reverserouter/ReverseCaller.java
+++ common/app/com/commercetools/sunrise/common/reverserouter/ReverseCaller.java
@@ -2,24 +2,6 @@ package com.commercetools.sunrise.common.reverserouter;
 
 import play.mvc.Call;
 
-import java.lang.reflect.Method;
-import java.util.concurrent.CompletionException;
-
-public class ReverseCaller {
-
-    private final Method method;
-    private final Object object;
-
-    public ReverseCaller(final Method method, final Object object) {
-        this.method = method;
-        this.object = object;
-    }
-
-    public Call call(final Object... args) {
-        try {
-            return (Call) method.invoke(object, args);
-        } catch (Exception e) {
-            throw new CompletionException(e);
-        }
-    }
+public interface ReverseCaller {
+    Call call(Object... args);
 }
