ecbd61fe629972ed57faef546484426463db0e0c
schleichardt <michael.schleichardt@commercetools.de>
2016-08-17 09:45:34 +0200

    docu ReverseCaller

--- common/app/com/commercetools/sunrise/common/reverserouter/ReverseCaller.java
+++ common/app/com/commercetools/sunrise/common/reverserouter/ReverseCaller.java
@@ -2,6 +2,10 @@ package com.commercetools.sunrise.common.reverserouter;
 
 import play.mvc.Call;
 
+/**
+ * Creates a {@link Call} like a reverse router.
+ */
+@FunctionalInterface
 public interface ReverseCaller {
-    Call call(Object... args);
+    Call call(Object ... args);
 }
