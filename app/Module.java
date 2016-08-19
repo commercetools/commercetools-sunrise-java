822b438121680546ed819490ef20ad451cd9cbde
Laura Luiz <laura.luiz@commercetools.de>
2016-08-19 15:28:24 +0200

    Remove components and request scope modules

--- app/Module.java
+++ app/Module.java
@@ -1,10 +1,18 @@
 import com.commercetools.sunrise.cms.CmsService;
+import com.commercetools.sunrise.common.localization.LocationSelectorControllerComponent;
+import com.commercetools.sunrise.common.pages.DefaultPageNavMenuControllerComponent;
 import com.commercetools.sunrise.common.template.cms.FileBasedCmsServiceProvider;
 import com.commercetools.sunrise.common.template.engine.HandlebarsTemplateEngineProvider;
 import com.commercetools.sunrise.common.template.engine.TemplateEngine;
 import com.commercetools.sunrise.common.template.i18n.ConfigurableI18nResolverProvider;
 import com.commercetools.sunrise.common.template.i18n.I18nResolver;
+import com.commercetools.sunrise.framework.MultiControllerComponentResolver;
+import com.commercetools.sunrise.framework.MultiControllerComponentResolverBuilder;
+import com.commercetools.sunrise.shoppingcart.MiniCartControllerComponent;
+import com.commercetools.sunrise.shoppingcart.common.CheckoutCommonComponent;
+import com.commercetools.sunrise.shoppingcart.common.CheckoutStepWidgetComponent;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 
 import javax.inject.Singleton;
 
@@ -15,5 +23,17 @@ public class Module extends AbstractModule {
         bind(CmsService.class).toProvider(FileBasedCmsServiceProvider.class).in(Singleton.class);
         bind(TemplateEngine.class).toProvider(HandlebarsTemplateEngineProvider.class).in(Singleton.class);
         bind(I18nResolver.class).toProvider(ConfigurableI18nResolverProvider.class).in(Singleton.class);
+    }
+
+    @Provides
+    public MultiControllerComponentResolver foo() {
+        //here are also instanceof checks possible
+        return new MultiControllerComponentResolverBuilder()
+                .add(CheckoutCommonComponent.class, controller -> controller.getFrameworkTags().contains("checkout"))
+                .add(CheckoutStepWidgetComponent.class, controller -> controller.getFrameworkTags().contains("checkout"))
+                .add(MiniCartControllerComponent.class, controller -> !controller.getFrameworkTags().contains("checkout"))
+                .add(DefaultPageNavMenuControllerComponent.class, controller -> !controller.getFrameworkTags().contains("checkout"))
+                .add(LocationSelectorControllerComponent.class, controller -> !controller.getFrameworkTags().contains("checkout"))
+                .build();
     }
 }
--- common/app/com/commercetools/sunrise/common/injection/SunriseModule.java
+++ common/app/com/commercetools/sunrise/common/injection/SunriseModule.java
@@ -1,19 +1,36 @@
 package com.commercetools.sunrise.common.injection;
 
+import com.commercetools.sunrise.common.contexts.RequestScope;
+import com.commercetools.sunrise.common.contexts.RequestScoped;
 import com.commercetools.sunrise.common.pages.RoutesMultiControllerComponentResolverProvider;
 import com.commercetools.sunrise.framework.MultiControllerComponentResolver;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
+import io.sphere.sdk.utils.MoneyImpl;
 
 import javax.inject.Singleton;
+import javax.money.Monetary;
+import javax.money.format.MonetaryFormats;
 
 public class SunriseModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        applyJavaMoneyHack();
+        bindScope(RequestScoped.class, new RequestScope());
         bind(MultiControllerComponentResolver.class)
                 .annotatedWith(Names.named("controllers"))
                 .toProvider(RoutesMultiControllerComponentResolverProvider.class)
                 .in(Singleton.class);
+    }
+
+    private void applyJavaMoneyHack() {
+        //fixes https://github.com/commercetools/commercetools-sunrise-java/issues/404
+        //exception play.api.http.HttpErrorHandlerExceptions$$anon$1: Execution exception[[CompletionException: java.lang.IllegalArgumentException: java.util.concurrent.CompletionException: io.sphere.sdk.json.JsonException: detailMessage: com.fasterxml.jackson.databind.JsonMappingException: Operator failed: javax.money.DefaultMonetaryRoundingsSingletonSpi$DefaultCurrencyRounding@1655879e (through reference chain: io.sphere.sdk.payments.PaymentDraftImpl["amountPlanned"])
+        Monetary.getDefaultRounding();
+        Monetary.getDefaultRounding().apply(MoneyImpl.ofCents(123, "EUR"));
+        Monetary.getDefaultAmountType();
+        MonetaryFormats.getDefaultFormatProviderChain();
+        Monetary.getDefaultCurrencyProviderChain();
     }
 }
