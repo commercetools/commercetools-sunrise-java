aa319d518780985ccc21192ade24de0efb136b3f
Laura Luiz <laura.luiz@commercetools.de>
2016-10-13 12:14:06 +0200

    Add documentation to base class

--- common/app/com/commercetools/sunrise/common/sessions/SessionHandlerBase.java
+++ common/app/com/commercetools/sunrise/common/sessions/SessionHandlerBase.java
@@ -8,10 +8,30 @@ import play.libs.Json;
 import play.mvc.Http;
 
 import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Class to use as a base to all session managers, with all common logic.
+ * @param <T> Class of the object in session
+ */
 public abstract class SessionHandlerBase<T> implements SessionHandler<T> {
+
+    /**
+     * Takes care of overwriting all related values to this object.
+     * For example, in the case of the cart, those might be overwriting the cart ID value, the mini cart instance, etc.
+     * @param session the HTTP session to be updated
+     * @param value the instance of the object used to update the session
+     */
+    protected abstract void overwriteRelatedValuesInSession(final Http.Session session, @NotNull final T value);
+
+    /**
+     * Takes care of overwriting all related values to this object.
+     * For example, in the case of the cart, those might be removing the cart ID value, the mini cart instance, etc.
+     * @param session the HTTP session to be updated
+     */
+    protected abstract void removeRelatedValuesFromSession(final Http.Session session);
 
     @Override
     public void overwriteInSession(final Http.Session session, @Nullable final T value) {
@@ -26,10 +46,6 @@ public abstract class SessionHandlerBase<T> implements SessionHandler<T> {
     public void removeFromSession(final Http.Session session) {
         removeRelatedValuesFromSession(session);
     }
-
-    protected abstract void overwriteRelatedValuesInSession(final Http.Session session, final T value);
-
-    protected abstract void removeRelatedValuesFromSession(final Http.Session session);
 
     protected Optional<String> findValueByKey(final Http.Session session, final String key) {
         return Optional.ofNullable(session.get(key));
