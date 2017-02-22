c84d7e7360c95d2262e4950fde9eac9ddd9756bf
Laura Luiz <laura.luiz@commercetools.de>
2017-02-22 19:00:58 +0100

    Fix Javadoc

--- common/app/com/commercetools/sunrise/framework/template/i18n/I18nIdentifier.java
+++ common/app/com/commercetools/sunrise/framework/template/i18n/I18nIdentifier.java
@@ -2,9 +2,6 @@ package com.commercetools.sunrise.framework.template.i18n;
 
 import io.sphere.sdk.models.Base;
 
-import static com.commercetools.sunrise.common.utils.ArrayUtils.getArrayElement;
-import static org.apache.commons.lang3.StringUtils.split;
-
 /**
  * i18n Identifier, consisting of the message key and the bundle.
  *
@@ -23,62 +20,15 @@ public final class I18nIdentifier extends Base {
         this.messageKey = messageKey;
     }
 
-    public String bundle() {
-        return bundle;
-    }
-
-    public String messageKey() {
-        return messageKey;
-    }
-
-    /**
-     * @deprecated use {@link #bundle} instead
-     * @return bundle
-     */
-    @Deprecated
     public String getBundle() {
         return bundle;
     }
 
-    /**
-     * @deprecated use {@link #messageKey} instead
-     * @return message key
-     */
-    @Deprecated
     public String getMessageKey() {
         return messageKey;
     }
 
-
     public static I18nIdentifier of(final String bundle, final String key) {
         return new I18nIdentifier(bundle, key);
-    }
-
-    /**
-     * Creates a i18n Identifier, consisting of the message key and the bundle.
-     * @param bundleWithKey of the form {@code bundle:key}
-     * @return the i18n Identifier for the given input
-     * @deprecated use {@link I18nIdentifierFactory} instead
-     */
-    @Deprecated
-    public static I18nIdentifier of(final String bundleWithKey) {
-        final I18nIdentifierFactory i18nIdentifierFactory = new I18nIdentifierFactory();
-        final String[] parts = split(bundleWithKey, ":", 2);
-        final String key = getArrayElement(parts, 1, bundleWithKey);
-        final String bundle;
-        if (bundleWithKey.equals(key)) {
-            bundle = i18nIdentifierFactory.getDefaultBundle();
-        } else {
-            bundle = getArrayElement(parts, 0, i18nIdentifierFactory.getDefaultBundle());
-        }
-        return ofBundleAndKey(bundle, key);
-    }
-
-    /**
-     * @deprecated use {@link #of} instead
-     */
-    @Deprecated
-    public static I18nIdentifier ofBundleAndKey(final String bundle, final String key) {
-        return of(bundle, key);
     }
 }
