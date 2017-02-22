8c12b844fb8f8ff86cfb0cfb0a6ed7aa3f316daf
Laura Luiz <laura.luiz@commercetools.de>
2017-02-22 17:02:10 +0100

    Use proper super class for form with void input

--- my-account/app/com/commercetools/sunrise/myaccount/mydetails/viewmodels/MyPersonalDetailsFormSettingsViewModelFactory.java
+++ my-account/app/com/commercetools/sunrise/myaccount/mydetails/viewmodels/MyPersonalDetailsFormSettingsViewModelFactory.java
@@ -1,15 +1,15 @@
 package com.commercetools.sunrise.myaccount.mydetails.viewmodels;
 
-import com.commercetools.sunrise.framework.injection.RequestScoped;
+import com.commercetools.sunrise.common.models.FormViewModelFactory;
 import com.commercetools.sunrise.common.models.addresses.TitleFormFieldViewModelFactory;
-import com.commercetools.sunrise.common.models.ViewModelFactory;
+import com.commercetools.sunrise.framework.injection.RequestScoped;
 import com.commercetools.sunrise.myaccount.mydetails.MyPersonalDetailsFormData;
 import play.data.Form;
 
 import javax.inject.Inject;
 
 @RequestScoped
-public class MyPersonalDetailsFormSettingsViewModelFactory extends ViewModelFactory<MyPersonalDetailsFormSettingsViewModel, Form<? extends MyPersonalDetailsFormData>> {
+public class MyPersonalDetailsFormSettingsViewModelFactory extends FormViewModelFactory<MyPersonalDetailsFormSettingsViewModel, Void, MyPersonalDetailsFormData> {
 
     private final TitleFormFieldViewModelFactory titleFormFieldViewModelFactory;
 
@@ -24,13 +24,18 @@ public class MyPersonalDetailsFormSettingsViewModelFactory extends ViewModelFact
     }
 
     @Override
+    public final MyPersonalDetailsFormSettingsViewModel create(final Void input, final Form<? extends MyPersonalDetailsFormData> form) {
+        return super.create(input, form);
+    }
+
     public final MyPersonalDetailsFormSettingsViewModel create(final Form<? extends MyPersonalDetailsFormData> form) {
-        return super.create(form);
+        return create(null, form);
     }
 
     @Override
-    protected final void initialize(final MyPersonalDetailsFormSettingsViewModel viewModel, final Form<? extends MyPersonalDetailsFormData> form) {
+    protected final void initialize(final MyPersonalDetailsFormSettingsViewModel viewModel, final Void input, final Form<? extends MyPersonalDetailsFormData> form) {
         fillTitle(viewModel, form);
+
     }
 
     protected void fillTitle(final MyPersonalDetailsFormSettingsViewModel viewModel, final Form<? extends MyPersonalDetailsFormData> form) {
