1c4ad2b13413ef0cf3684512c930727dfa8508a1
Laura Luiz <laura.luiz@commercetools.de>
2017-03-21 00:49:55 +0100

    Add documentation to Module

--- app/Module.java
+++ app/Module.java
@@ -40,6 +40,16 @@ import java.util.concurrent.TimeUnit;
 import static io.sphere.sdk.client.SphereClientUtils.blockingWait;
 import static io.sphere.sdk.queries.QueryExecutionUtils.queryAll;
 
+/**
+ * This class is a Guice module that tells Guice how to bind several
+ * different types. This Guice module is created when the Play
+ * application starts.
+ *
+ * Play will automatically use any class called `Module` that is in
+ * the root package. You can create modules in other locations by
+ * adding `play.modules.enabled` settings to the `application.conf`
+ * configuration file.
+ */
 public class Module extends AbstractModule {
 
     @Override
