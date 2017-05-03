29504c78a5cb64aa62282c321351488bec4ca0be
Matthias Köster <katmatt@users.noreply.github.com>
2017-05-03 16:56:21 +0200

    Add missing controller component to page header component supplier. (#597)

--- common/app/com/commercetools/sunrise/framework/components/controllers/PageHeaderControllerComponentSupplier.java
+++ common/app/com/commercetools/sunrise/framework/components/controllers/PageHeaderControllerComponentSupplier.java
@@ -2,6 +2,7 @@ package com.commercetools.sunrise.framework.components.controllers;
 
 import com.commercetools.sunrise.common.localization.LocationSelectorControllerComponent;
 import com.commercetools.sunrise.framework.viewmodels.header.PageNavMenuControllerComponent;
+import com.commercetools.sunrise.sessions.customer.CustomerInSessionControllerComponent;
 
 import javax.inject.Inject;
 
@@ -10,9 +11,11 @@ public class PageHeaderControllerComponentSupplier extends AbstractControllerCom
     @Inject
     public PageHeaderControllerComponentSupplier(final MiniCartControllerComponent miniCartControllerComponent,
                                                  final PageNavMenuControllerComponent pageNavMenuControllerComponent,
-                                                 final LocationSelectorControllerComponent locationSelectorControllerComponent) {
+                                                 final LocationSelectorControllerComponent locationSelectorControllerComponent,
+                                                 final CustomerInSessionControllerComponent customerInSessionControllerComponent) {
         add(miniCartControllerComponent);
         add(pageNavMenuControllerComponent);
         add(locationSelectorControllerComponent);
+        add(customerInSessionControllerComponent);
     }
 }
