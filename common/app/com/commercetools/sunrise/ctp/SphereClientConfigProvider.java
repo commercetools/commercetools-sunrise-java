242338e1590d77dd7b22ef8df7491ff94c0e1bae
Beniasaad Achraf <beni.asaad.achraf@gmail.com>
2017-09-15 18:30:47 +0200

    Customizing client config scopes (#661)
    Closes #612

--- common/app/com/commercetools/sunrise/ctp/SphereClientConfigProvider.java
+++ common/app/com/commercetools/sunrise/ctp/SphereClientConfigProvider.java
@@ -2,9 +2,11 @@ package com.commercetools.sunrise.ctp;
 
 import com.google.inject.Provider;
 import io.sphere.sdk.client.SphereClientConfig;
+import io.sphere.sdk.client.SphereClientConfigBuilder;
 import play.Configuration;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.Optional;
 
 public final class SphereClientConfigProvider implements Provider<SphereClientConfig> {
@@ -14,6 +16,7 @@ public final class SphereClientConfigProvider implements Provider<SphereClientCo
     private static final String CONFIG_CLIENT_SECRET = "ctp.clientSecret";
     private static final String CONFIG_API_URL = "ctp.apiUrl";
     private static final String CONFIG_AUTH_URL = "ctp.authUrl";
+    private static final String CONFIG_SCOPES = "ctp.scopes";
     private static final String DEFAULT_API_URL = "https://api.sphere.io";
     private static final String DEFAULT_AUTH_URL = "https://auth.sphere.io";
 
@@ -22,6 +25,7 @@ public final class SphereClientConfigProvider implements Provider<SphereClientCo
     private final String clientSecret;
     private final String authUrl;
     private final String apiUrl;
+    private final List<String> scopes;
 
     @Inject
     public SphereClientConfigProvider(final Configuration configuration) {
@@ -30,15 +34,22 @@ public final class SphereClientConfigProvider implements Provider<SphereClientCo
         this.clientSecret = getValue(configuration, CONFIG_CLIENT_SECRET);
         this.authUrl = configuration.getString(CONFIG_AUTH_URL, DEFAULT_AUTH_URL);
         this.apiUrl = configuration.getString(CONFIG_API_URL, DEFAULT_API_URL);
+        this.scopes = configuration.getStringList(CONFIG_SCOPES);
     }
 
     @Override
     public SphereClientConfig get() {
-        return SphereClientConfig.of(project, clientId, clientSecret, authUrl, apiUrl);
+        return SphereClientConfigBuilder.ofKeyIdSecret(project, clientId, clientSecret)
+                .authUrl(authUrl)
+                .apiUrl(apiUrl)
+                .scopeStrings(scopes)
+                .build();
     }
 
     private String getValue(final Configuration configuration, final String key) {
         return Optional.ofNullable(configuration.getString(key))
                 .orElseThrow(() -> new SphereClientCredentialsException("Missing value for configuration key " + key));
     }
+
+
 }
