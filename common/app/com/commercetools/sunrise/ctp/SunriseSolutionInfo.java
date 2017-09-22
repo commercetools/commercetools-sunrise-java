86f626d70c5c9c42792793d43baceddb10fd878f
Laura Luiz <laura.luiz@commercetools.de>
2017-09-22 14:56:52 +0200

    Make SunriseSolutionInfo final and document how to provide your own (#670)

--- common/app/com/commercetools/sunrise/ctp/SunriseSolutionInfo.java
+++ common/app/com/commercetools/sunrise/ctp/SunriseSolutionInfo.java
@@ -2,7 +2,13 @@ package com.commercetools.sunrise.ctp;
 
 import io.sphere.sdk.client.SolutionInfo;
 
-public class SunriseSolutionInfo extends SolutionInfo {
+/**
+ * Information that is going to be sent to the commercetools platform on each request via the User-Agent header.
+ *
+ * If you want to provide additional solution info, you can create your own class in your project extending from
+ * {@link SolutionInfo} and following the documentation described in that class.
+ */
+public final class SunriseSolutionInfo extends SolutionInfo {
 
     public SunriseSolutionInfo(){
         setName("sunrise-java-shop-framework");
