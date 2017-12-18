d5b08c4741022493451f65901552794106f06d1f
Laura Luiz <laura.luiz@commercetools.de>
2017-12-18 13:28:07 +0100

    Add attribute label helper

--- common/app/com/commercetools/sunrise/core/renderers/handlebars/ProductHelperSource.java
+++ common/app/com/commercetools/sunrise/core/renderers/handlebars/ProductHelperSource.java
@@ -1,9 +1,11 @@
 package com.commercetools.sunrise.core.renderers.handlebars;
 
+import com.commercetools.sunrise.core.i18n.I18nResolver;
 import com.commercetools.sunrise.core.viewmodels.formatters.AttributeFormatter;
 import com.commercetools.sunrise.models.products.AttributeWithProductType;
 import com.commercetools.sunrise.models.products.ProductAttributesSettings;
 import com.github.jknack.handlebars.Options;
+import io.sphere.sdk.models.LocalizedString;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.products.ProductVariant;
 import io.sphere.sdk.products.attributes.Attribute;
@@ -24,11 +26,14 @@ public class ProductHelperSource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductHelperSource.class);
 
+    private final I18nResolver i18nResolver;
     private final AttributeFormatter attributeFormatter;
     private final ProductAttributesSettings productAttributesSettings;
 
     @Inject
-    ProductHelperSource(final AttributeFormatter attributeFormatter, final ProductAttributesSettings productAttributesSettings) {
+    ProductHelperSource(final I18nResolver i18nResolver, final AttributeFormatter attributeFormatter,
+                        final ProductAttributesSettings productAttributesSettings) {
+        this.i18nResolver = i18nResolver;
         this.attributeFormatter = attributeFormatter;
         this.productAttributesSettings = productAttributesSettings;
     }
@@ -39,6 +44,11 @@ public class ProductHelperSource {
                 .map(attribute -> applyTemplateFunction(attribute, options))
                 .filter(Objects::nonNull)
                 .collect(joining());
+    }
+
+    public CharSequence printAttributeLabel(final Attribute attribute, final Reference<ProductType> productTypeRef) {
+        final LocalizedString label = attributeFormatter.label(AttributeWithProductType.of(attribute, productTypeRef));
+        return i18nResolver.get(label).orElse("");
     }
 
     public CharSequence printAttributeValue(final Attribute attribute, final Reference<ProductType> productTypeRef) {
