5d2f100b9d1c5da1a0633692e771d8a481a8e033
Laura Luiz <laura.luiz@commercetools.de>
2017-12-20 00:57:23 +0100

    Remove unnecessary files

--- common/app/com/commercetools/sunrise/models/products/ProductViewModel.java
+++ common/app/com/commercetools/sunrise/models/products/ProductViewModel.java
@@ -1,21 +1,13 @@
 package com.commercetools.sunrise.models.products;
 
 import com.commercetools.sunrise.core.viewmodels.ViewModel;
-import io.sphere.sdk.models.LocalizedString;
-
-import java.util.List;
 
 public class ProductViewModel extends ViewModel {
 
     private String productId;
     private int variantId;
-    private LocalizedString description;
     private ProductGalleryViewModel gallery;
-    private List<SelectableProductAttributeViewModel> attributes;
     private ProductVariantViewModel variant;
-    private ProductVariantReferenceMapViewModel variants;
-    private List<String> variantIdentifiers;
-    private ProductDetailsViewModel details;
     private String availability;
 
     public ProductViewModel() {
@@ -37,14 +29,6 @@ public class ProductViewModel extends ViewModel {
         this.variantId = variantId;
     }
 
-    public LocalizedString getDescription() {
-        return description;
-    }
-
-    public void setDescription(final LocalizedString description) {
-        this.description = description;
-    }
-
     public ProductGalleryViewModel getGallery() {
         return gallery;
     }
@@ -53,44 +37,12 @@ public class ProductViewModel extends ViewModel {
         this.gallery = gallery;
     }
 
-    public List<SelectableProductAttributeViewModel> getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(final List<SelectableProductAttributeViewModel> attributes) {
-        this.attributes = attributes;
-    }
-
     public ProductVariantViewModel getVariant() {
         return variant;
     }
 
     public void setVariant(final ProductVariantViewModel variant) {
         this.variant = variant;
-    }
-
-    public ProductVariantReferenceMapViewModel getVariants() {
-        return variants;
-    }
-
-    public void setVariants(final ProductVariantReferenceMapViewModel variants) {
-        this.variants = variants;
-    }
-
-    public List<String> getVariantIdentifiers() {
-        return variantIdentifiers;
-    }
-
-    public void setVariantIdentifiers(final List<String> variantIdentifiers) {
-        this.variantIdentifiers = variantIdentifiers;
-    }
-
-    public ProductDetailsViewModel getDetails() {
-        return details;
-    }
-
-    public void setDetails(final ProductDetailsViewModel details) {
-        this.details = details;
     }
 
     public String getAvailability() {
--- common/app/com/commercetools/sunrise/models/products/ProductViewModelFactory.java
+++ common/app/com/commercetools/sunrise/models/products/ProductViewModelFactory.java
@@ -9,48 +9,11 @@ import java.util.Optional;
 @RequestScoped
 public class ProductViewModelFactory extends SimpleViewModelFactory<ProductViewModel, ProductWithVariant> {
 
-    private final ProductAttributesSettings productAttributesSettings;
     private final ProductVariantViewModelFactory productVariantViewModelFactory;
-    private final ProductDetailsViewModelFactory productDetailsViewModelFactory;
-    private final ProductGalleryViewModelFactory productGalleryViewModelFactory;
-    private final SelectableProductAttributeViewModelFactory selectableProductAttributeViewModelFactory;
-    private final ProductVariantReferenceViewModelMapFactory productVariantReferenceViewModelMapFactory;
 
     @Inject
-    public ProductViewModelFactory(final ProductAttributesSettings productAttributesSettings, final ProductVariantViewModelFactory productVariantViewModelFactory,
-                                   final ProductDetailsViewModelFactory productDetailsViewModelFactory, final ProductGalleryViewModelFactory productGalleryViewModelFactory,
-                                   final SelectableProductAttributeViewModelFactory selectableProductAttributeViewModelFactory,
-                                   final ProductVariantReferenceViewModelMapFactory productVariantReferenceViewModelMapFactory) {
-        this.productAttributesSettings = productAttributesSettings;
+    public ProductViewModelFactory(final ProductVariantViewModelFactory productVariantViewModelFactory) {
         this.productVariantViewModelFactory = productVariantViewModelFactory;
-        this.productDetailsViewModelFactory = productDetailsViewModelFactory;
-        this.productGalleryViewModelFactory = productGalleryViewModelFactory;
-        this.selectableProductAttributeViewModelFactory = selectableProductAttributeViewModelFactory;
-        this.productVariantReferenceViewModelMapFactory = productVariantReferenceViewModelMapFactory;
-    }
-
-    protected final ProductAttributesSettings getProductAttributesSettings() {
-        return productAttributesSettings;
-    }
-
-    protected final ProductVariantViewModelFactory getProductVariantViewModelFactory() {
-        return productVariantViewModelFactory;
-    }
-
-    protected final ProductDetailsViewModelFactory getProductDetailsViewModelFactory() {
-        return productDetailsViewModelFactory;
-    }
-
-    protected final ProductGalleryViewModelFactory getProductGalleryViewModelFactory() {
-        return productGalleryViewModelFactory;
-    }
-
-    protected final SelectableProductAttributeViewModelFactory getSelectableProductAttributeViewModelFactory() {
-        return selectableProductAttributeViewModelFactory;
-    }
-
-    protected final ProductVariantReferenceViewModelMapFactory getProductVariantReferenceViewModelMapFactory() {
-        return productVariantReferenceViewModelMapFactory;
     }
 
     @Override
@@ -67,14 +30,8 @@ public class ProductViewModelFactory extends SimpleViewModelFactory<ProductViewM
     protected final void initialize(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
         fillProductId(viewModel, productWithVariant);
         fillVariantId(viewModel, productWithVariant);
-        fillDescription(viewModel, productWithVariant);
-        fillGallery(viewModel, productWithVariant);
-        fillDetails(viewModel, productWithVariant);
         fillVariant(viewModel, productWithVariant);
-        fillAttributes(viewModel, productWithVariant);
         fillAvailability(viewModel, productWithVariant);
-        fillVariantIdentifiers(viewModel, productWithVariant);
-        fillAttributeCombination(viewModel, productWithVariant);
     }
 
     protected void fillProductId(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
@@ -85,32 +42,8 @@ public class ProductViewModelFactory extends SimpleViewModelFactory<ProductViewM
         viewModel.setVariantId(productWithVariant.getVariant().getId());
     }
 
-    protected void fillDescription(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
-        viewModel.setDescription(productWithVariant.getProduct().getDescription());
-    }
-
-    protected void fillGallery(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
-        viewModel.setGallery(productGalleryViewModelFactory.create(productWithVariant.getVariant()));
-    }
-
-    protected void fillDetails(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
-        viewModel.setDetails(productDetailsViewModelFactory.create(productWithVariant));
-    }
-
     protected void fillVariant(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
         viewModel.setVariant(productVariantViewModelFactory.create(productWithVariant));
-    }
-
-    protected void fillAttributes(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
-        viewModel.setAttributes(selectableProductAttributeViewModelFactory.createList(productWithVariant));
-    }
-
-    protected void fillAttributeCombination(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
-        viewModel.setVariants(productVariantReferenceViewModelMapFactory.create(productWithVariant));
-    }
-
-    protected void fillVariantIdentifiers(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
-        viewModel.setVariantIdentifiers(productAttributesSettings.selectable());
     }
 
     protected void fillAvailability(final ProductViewModel viewModel, final ProductWithVariant productWithVariant) {
