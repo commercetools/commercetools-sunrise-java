6cfd033422492a309139ecda1fedf35930ea9793
schleichardt <michael.schleichardt@commercetools.de>
2017-12-19 15:58:07 +0100

    refactor title change address

--- my-account/app/com/commercetools/sunrise/myaccount/addressbook/changeaddress/viewmodels/ChangeAddressPageContentFactory.java
+++ my-account/app/com/commercetools/sunrise/myaccount/addressbook/changeaddress/viewmodels/ChangeAddressPageContentFactory.java
@@ -1,8 +1,7 @@
 package com.commercetools.sunrise.myaccount.addressbook.changeaddress.viewmodels;
 
-import com.commercetools.sunrise.models.addresses.AddressWithCustomer;
 import com.commercetools.sunrise.core.viewmodels.content.FormPageContentFactory;
-import com.commercetools.sunrise.core.viewmodels.PageTitleResolver;
+import com.commercetools.sunrise.models.addresses.AddressWithCustomer;
 import com.commercetools.sunrise.myaccount.addressbook.AddressFormData;
 import com.commercetools.sunrise.myaccount.addressbook.addressbookdetail.viewmodels.AddressFormSettingsViewModelFactory;
 import play.data.Form;
@@ -11,17 +10,11 @@ import javax.inject.Inject;
 
 public class ChangeAddressPageContentFactory extends FormPageContentFactory<ChangeAddressPageContent, AddressWithCustomer, AddressFormData> {
 
-    private final PageTitleResolver pageTitleResolver;
     private final AddressFormSettingsViewModelFactory addressFormSettingsFactory;
 
     @Inject
-    public ChangeAddressPageContentFactory(final PageTitleResolver pageTitleResolver, final AddressFormSettingsViewModelFactory addressFormSettingsFactory) {
-        this.pageTitleResolver = pageTitleResolver;
+    public ChangeAddressPageContentFactory(final AddressFormSettingsViewModelFactory addressFormSettingsFactory) {
         this.addressFormSettingsFactory = addressFormSettingsFactory;
-    }
-
-    protected final PageTitleResolver getPageTitleResolver() {
-        return pageTitleResolver;
     }
 
     protected final AddressFormSettingsViewModelFactory getAddressFormSettingsFactory() {
@@ -43,11 +36,6 @@ public class ChangeAddressPageContentFactory extends FormPageContentFactory<Chan
         super.initialize(viewModel, addressWithCustomer, form);
         fillEditAddressForm(viewModel, addressWithCustomer, form);
         fillEditAddressFormSettings(viewModel, addressWithCustomer, form);
-    }
-
-    @Override
-    protected void fillTitle(final ChangeAddressPageContent viewModel, final AddressWithCustomer addressWithCustomer, final Form<? extends AddressFormData> form) {
-        viewModel.setTitle(pageTitleResolver.getOrEmpty("my-account:changeAddressPage.title"));
     }
 
     protected void fillEditAddressForm(final ChangeAddressPageContent viewModel, final AddressWithCustomer addressWithCustomer, final Form<? extends AddressFormData> form) {
