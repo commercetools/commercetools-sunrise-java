2d888fd2c069ac91db691fa23f923a79f18a2c2f
schleichardt <michael.schleichardt@commercetools.de>
2017-12-21 11:16:57 +0100

    remove unused classes

--- common/app/com/commercetools/sunrise/models/search/facetedsearch/FacetedSearchFormSettingsWithOptions.java
+++
@@ -1,5 +0,0 @@
-package com.commercetools.sunrise.models.search.facetedsearch;
-
-public interface FacetedSearchFormSettingsWithOptions<T> extends FacetedSearchFormSettings<T>, BaseFacetedSearchWithOptions {
-
-}
