2686a4d9969da1e379365ef5ff691eb81e046375
Laura Luiz <laura.luiz@commercetools.de>
2018-01-14 14:14:14 +0100

    Copy all templates

--- common/app/com/commercetools/sunrise/controllers/LocalizationController.java
+++ common/app/com/commercetools/sunrise/controllers/LocalizationController.java
@@ -4,6 +4,8 @@ import com.commercetools.sunrise.core.SunriseController;
 import com.commercetools.sunrise.core.hooks.EnableHooks;
 import com.commercetools.sunrise.core.localization.CountryInSession;
 import com.neovisionaries.i18n.CountryCode;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.i18n.Lang;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -15,16 +17,19 @@ import java.util.Locale;
 @Singleton
 public final class LocalizationController extends SunriseController {
 
+    private final FormFactory formFactory;
     private final CountryInSession countryInSession;
 
     @Inject
-    private LocalizationController(final CountryInSession countryInSession) {
+    LocalizationController(final FormFactory formFactory, final CountryInSession countryInSession) {
+        this.formFactory = formFactory;
         this.countryInSession = countryInSession;
     }
 
     @EnableHooks
-    public Result changeLanguage(final String language) {
-        final Lang lang = new Lang(Locale.forLanguageTag(language));
+    public Result changeLanguage() {
+        final DynamicForm form = formFactory.form().bindFromRequest();
+        final Lang lang = new Lang(Locale.forLanguageTag(form.get("language")));
         if (!changeLang(lang)) {
             LOGGER.debug("Could not change language to '{}'", lang);
         }
@@ -32,8 +37,9 @@ public final class LocalizationController extends SunriseController {
     }
 
     @EnableHooks
-    public Result changeCountry(final String country) {
-        final CountryCode countryCode = CountryCode.getByCode(country);
+    public Result changeCountry() {
+        final DynamicForm form = formFactory.form().bindFromRequest();
+        final CountryCode countryCode = CountryCode.getByCode(form.get("country"));
         countryInSession.store(countryCode);
         return redirect(request().getHeader(Http.HeaderNames.REFERER));
     }
